0e3c29c3e1c12a127e633b4f854ff2d36bd07e4b
lishun <773538995@qq.com>
2016-05-20 15:38:22 +0800

    重复提交

--- iShop/src/main/webapp/js/login.js
+++ iShop/src/main/webapp/js/login.js
@@ -7,8 +7,19 @@ $(function(){
 	    param["phone"]=phone;
 	    param["password"]=password;
 	    console.log(param);
+	    if(phone==''||password==''){
+	    	if(phone==""){
+	    		$(".portlet-msg-error").html("用户名不能为空");
+	    	}
+	    	if(password==''){
+	    		$(".portlet-msg-error").html("密码不能为空");
+	    	}
+	    	return;	
+	    }
 	    oc.postRequire("post","/login","0",param,function(data){
-	    	console.log(data);
+	    	if(data.code=="0"){
+	    		
+	    	}
 	    })
 	})
 })
