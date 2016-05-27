29dbd24c470d06793e8b5cd622fd7551c8ed8f61
7x7 <1406900537@qq.com>
2016-05-27 21:55:57 +0800

    no message

--- iShop/src/main/webapp/js/user/user_operate.js
+++ iShop/src/main/webapp/js/user/user_operate.js
@@ -174,6 +174,8 @@ jQuery(document).ready(function(){
 		console.log(data);
 		if(data.code=="0"){
 			var msg=data.message;
+			console.log(msg);
+			console.log(msg.user_code);
 			$("#ACCOUNT").val(msg.user_code);
 			$("#USER_NAME").val(msg.username);
 			$("#preview img").attr("src",msg.avater);
