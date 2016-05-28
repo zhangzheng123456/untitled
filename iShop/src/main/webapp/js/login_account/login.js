8aab77b21ba1b3b2b8731615a10fe0618744ef82
lishun <773538995@qq.com>
2016-05-28 21:41:06 +0800

    页面修改

--- iShop/src/main/webapp/js/login_account/login.js
+++ iShop/src/main/webapp/js/login_account/login.js
@@ -1,6 +1,7 @@
 var oc = new ObjectControl();
 var code;
-function createCode() {//随机生成验证码
+
+function createCode() { //随机生成验证码
 	code = "";
 	var codeLength = 4; //验证码的长度
 	var checkCode = document.getElementById("checkCode");
@@ -17,54 +18,62 @@ function createCode() {//随机生成验证码
 	}
 }
 createCode();
-$(function() {//点击登陆
-	$('.btn_login').click(function() {
-		var phone = $('#login').val();
-		var password = $('#password1').val();
-		var verifyCode = $('#verifyCode').val();
-		var param = {};
-		param["phone"] = phone;
-		param["password"] = password;
-		console.log(param);
-		console.log(verifyCode.toUpperCase());
-		console.log(code.toUpperCase());
-		// if (phone== ''|| password ==''|| verifyCode==''||verifyCode!==''&&verifyCode.toUpperCase()!==code.toUpperCase()) {
-		// 	if(phone=="") {
-		// 		$(".portlet-msg-error").html("用户名不能为空");
-		// 	}
-		// 	if(password=='') {
-		// 		$(".portlet-msg-error").html("密码不能为空");
-		// 	}
-		// 	if(verifyCode==''){
-		// 		$(".portlet-msg-error").html("验证码不能为空");
-		// 	}
-		// 	if(verifyCode!==''&&verifyCode.toUpperCase()!==code.toUpperCase()){
-		// 		$(".portlet-msg-error").html("验证码不正确");	
-		// 	}
-		// 	return;
-		// }
-		oc.postRequire("post","/userlogin","0", param, function(data) {
-			var str = JSON.stringify(data);
-			var key="key";
-			sessionStorage.setItem(key,str);
-			if(data.code=="0"){
-				var message=JSON.parse(data.message);
-                var user_type=message.user_type;
-                var user_id=message.user_id;
-                var val=message.menu;
-                console.log(message.menu);
-				if(user_type=="admin"){
-                	window.location.href="home/index_admin.html";
-				}else if(user_type=="am"){
-					window.location.href="home/index_am.html";
-				}else if(user_type=="gm"){
-					window.location.href="home/index_gm.html";
-				}else if(user_type=="staff"){
-					window.location.href="home/index_staff.html";
-				}
-			}else if(data.code=="-1"){
-				$(".portlet-msg-error").html("用户名或密码错误");
+
+function login(){
+	var phone = $('#login').val();
+	var password = $('#password1').val();
+	var verifyCode = $('#verifyCode').val();
+	var param = {};
+	param["phone"] = phone;
+	param["password"] = password;
+	if (phone == '' || password == '' || verifyCode == '' || verifyCode !== '' && verifyCode.toUpperCase() !== code.toUpperCase()) {
+		if (phone == "") {
+			$(".portlet-msg-error").html("用户名不能为空");
+		}
+		if (password == '') {
+			$(".portlet-msg-error").html("密码不能为空");
+		}
+		if (verifyCode == '') {
+			$(".portlet-msg-error").html("验证码不能为空");
+		}
+		if (verifyCode !== '' && verifyCode.toUpperCase() !== code.toUpperCase()) {
+			$(".portlet-msg-error").html("验证码不正确");
+		}
+		return;
+	}
+	oc.postRequire("post", "/userlogin", "0", param, function(data) {
+		console.log(data);
+		var str = JSON.stringify(data);
+		var key = "key";
+		sessionStorage.setItem(key, str);
+		if (data.code == "0") {
+			var message = JSON.parse(data.message);
+			var user_type = message.user_type;
+			var user_id = message.user_id;
+			var val = message.menu;
+			console.log(message.menu);
+			if (user_type == "admin") {
+				window.location.href = "home/index_admin.html";
+			} else if (user_type == "am") {
+				window.location.href = "home/index_am.html";
+			} else if (user_type == "gm") {
+				window.location.href = "home/index_gm.html";
+			} else if (user_type == "staff") {
+				window.location.href = "home/index_staff.html";
 			}
-		})
+		} else if (data.code == "-1") {
+			$(".portlet-msg-error").html("用户名或密码错误");
+		}
 	})
+}
+$(function() { //点击登陆
+	$('.btn_login').click(function() {
+		login();
+	})
+	$(window).keydown(function() {
+		var event = window.event || arguments[0];
+		if (event.keyCode == 13) {
+			login();
+		}
+	});
 })
