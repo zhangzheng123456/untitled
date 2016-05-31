f90070e9468b91227d39baefd0eca761904ffbaa
7x7 <1406900537@qq.com>
2016-05-31 17:04:10 +0800

    no message

--- iShop/src/main/webapp/js/user/user_operate.js
+++ iShop/src/main/webapp/js/user/user_operate.js
@@ -357,7 +357,7 @@ jQuery(document).ready(function(){
 							html +='<div>'
 					            +'<span style="display:inline-block;" data-i="1" onclick="selectownshop(this)">'
 					                +'<input class="input_select"  type="text" value="'+store_lists[i]+'" placeholder="请选择所属店铺" readonly/><span class="down_icon "><i class="icon-ishop_8-02"></i></span>'
-					                +'<ul style="margin-left:0px">'
+					                +'<ul style="margin-left:0px" id="store_list">'
 					                +'</ul>'
 					            +'</span>'
 					            +' <span class="minus_per_icon" onclick="minusshopselect(this)"><i class="icon-ishop_6-12"></i>删除店铺</span>'
