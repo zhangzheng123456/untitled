49d3ebfdb2a2961eba074bf2b296634e69c68073
lishun <773538995@qq.com>
2016-06-21 20:46:26 +0800

    样式

--- iShop/src/main/webapp/js/corp/corp.js
+++ iShop/src/main/webapp/js/corp/corp.js
@@ -8,9 +8,6 @@ var param={};//定义的对象
 var key_val=sessionStorage.getItem("key_val");//取页面的function_code
 key_val=JSON.parse(key_val);
 var funcCode=key_val.func_code;
-//模仿select
-
-///aasd 
 $(function(){  
         $("#page_row").click(function(){
 
