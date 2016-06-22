2900d68c75a7fef843f35ef82a0260ceda4c3dc1
liuying8133 <liuying8133@163.com>
2016-06-22 17:16:39 +0800

    列表

--- iShop/src/main/java/com/bizvane/ishop/controller/FeedbackController.java
+++ iShop/src/main/java/com/bizvane/ishop/controller/FeedbackController.java
@@ -35,9 +35,39 @@ public class FeedbackController {
 
     SimpleDateFormat sdf = new SimpleDateFormat(Common.DATE_FORMATE);
     private static final Logger logger = Logger.getLogger(FeedbackController.class);
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+   //列表
+    public String selectAll(HttpServletRequest request) {
+        DataBean dataBean = new DataBean();
+        try {
+            String jsString = request.getParameter("param");
+            JSONObject jsonObj = new JSONObject(jsString);
+            id = jsonObj.get("id").toString();
+            String message = jsonObj.get("message").toString();
+            JSONObject jsonObject = new JSONObject(message);
+            //-------------------------------------------------------
+            int page_number = Integer.valueOf(jsonObject.get("pageNumber").toString());
+            int page_size = Integer.valueOf(jsonObject.get("pageSize").toString());
+            //String search_value = jsonObject.get("searchValue").toString();
+            JSONObject result = new JSONObject();
+            PageInfo<Feedback> list = feedbackService.selectAllFeedback(page_number, page_size, "");
+            result.put("list", JSON.toJSONString(list));
+            dataBean.setCode(Common.DATABEAN_CODE_SUCCESS);
+            dataBean.setId(id);
+            dataBean.setMessage(result.toString());
+        } catch (Exception ex) {
+            dataBean.setCode(Common.DATABEAN_CODE_ERROR);
+            dataBean.setId(id);
+            dataBean.setMessage(ex.getMessage());
+        }
+        return dataBean.getJsonStr();
+    }
+
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     @ResponseBody
-    //查询分页
+    //条件查询
     public String search(HttpServletRequest request) {
         DataBean dataBean = new DataBean();
         try {
