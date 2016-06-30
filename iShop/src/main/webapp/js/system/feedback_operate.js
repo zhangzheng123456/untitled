b076ccdbea9698182b2227a287ad05c8bfaf990a
王+2 <1194907581@qq.com>
2016-06-30 17:03:53 +0800

    反馈修改

--- iShop/src/main/webapp/js/system/feedback_operate.js
+++ iShop/src/main/webapp/js/system/feedback_operate.js
@@ -1,16 +1,16 @@
 var oc = new ObjectControl();
 (function(root,factory){
-	root.interface = factory();
+	root.feedback = factory();
 }(this,function(){
-	var interfacejs={};
-	interfacejs.isEmpty=function(obj){
+	var feedbackjs={};
+	feedbackjs.isEmpty=function(obj){
 		if(obj.trim() == "" || obj.trim() == undefined){
 			return true;
 		}else{
 			return false;
 		}
 	};
-	interfacejs.checkEmpty = function(obj,hint){
+	feedbackjs.checkEmpty = function(obj,hint){
 		if(!this.isEmpty(obj)){
 			this.hiddenHint(hint);
 			return true;
@@ -19,27 +19,30 @@ var oc = new ObjectControl();
 			return false;
 		}
 	};
-	interfacejs.hiddenHint = function(hint){
+	feedbackjs.hiddenHint = function(hint){
 		hint.removeClass('error_tips');
 		hint.html("");//关闭，如果有友情提示则显示
 	};
-	interfacejs.displayHint = function(hint,content){
+	feedbackjs.displayHint = function(hint,content){
 		hint.addClass("error_tips");
 		if(!content)hint.html(hint.attr("hintInfo"));//错误提示
 		else hint.html(content);
 	};
-	interfacejs.firstStep = function(){
+	feedbackjs.firstStep = function(){
 		var inputText = jQuery(".conpany_msg").find(":text");
 		for(var i=0,length=inputText.length;i<length;i++){
 			if(!bindFun(inputText[i]))return false;
 		}
 		return true;
 	};
-	interfacejs.bindbutton=function(){
+	feedbackjs.bindbutton=function(){
 		$(".operadd_btn ul li:nth-of-type(1)").click(function(){
-			if(interfacejs.firstStep()){
-				var INTERFACE_VERSION=$("#INTERFACE_VERSION").val();
-				var CORP_ID=$("#CORP_ID").val();
+			if(feedbackjs.firstStep()){
+				var USER_CODE=$("#USER_CODE").val();
+				var FEEDBACK_DATE=$("#FEEDBACK_DATE").val();
+				var PHONE=$("#PHONE").val();
+				var FEEDBACK_CONTENT=$("#FEEDBACK_CONTENT").val();
+				var PROCESS_STATE=$("#PROCESS_STATE").val();
 				var ISACTIVE="";
 				var input=$(".checkbox_isactive").find("input")[0];
 				if(input.checked==true){
@@ -47,22 +50,26 @@ var oc = new ObjectControl();
 				}else if(input.checked==false){
 					ISACTIVE="N";
 				}
-				var _command="/interfacers/add";//接口名
+				var _command="/feedback/add";//接口名
 				var opt = {//返回成功后的操作
 					success:function(){
 					}
 				};
-				var _params={"version":INTERFACE_VERSION,"corp_code":CORP_ID,"isactive":ISACTIVE};
-				interfacejs.ajaxSubmit(_command,_params,opt);
+				var _params={"user_code":USER_CODE,"feedback_date":FEEDBACK_DATE,"phone":PHONE,
+				"feedback_content":FEEDBACK_CONTENT,"process_state":PROCESS_STATE,"isactive":ISACTIVE};
+				feedbackjs.ajaxSubmit(_command,_params,opt);
 			}else{
 				return;
 			}
 		});
 		$(".operedit_btn ul li:nth-of-type(1)").click(function(){
-			if(interfacejs.firstStep()){
+			if(feedbackjs.firstStep()){
 				var ID=sessionStorage.getItem("id");
-				var INTERFACE_VERSION=$("#INTERFACE_VERSION").val();
-				var CORP_ID=$("#CORP_ID").val();
+				var USER_CODE=$("#USER_CODE").val();
+				var FEEDBACK_DATE=$("#FEEDBACK_DATE").val();
+				var PHONE=$("#PHONE").val();
+				var FEEDBACK_CONTENT=$("#FEEDBACK_CONTENT").val();
+				var PROCESS_STATE=$("#PROCESS_STATE").val();
 				var ISACTIVE="";
 				var input=$(".checkbox_isactive").find("input")[0];
 				if(input.checked==true){
@@ -70,20 +77,21 @@ var oc = new ObjectControl();
 				}else if(input.checked==false){
 					ISACTIVE="N";
 				}
-				var _command="/interfacers/edit";//接口名
+				var _command="/feedback/edit";//接口名
 				var opt = {//返回成功后的操作
 					success:function(){
 
 					}
 				};
-				var _params={"id":ID,"version":INTERFACE_VERSION,"corp_code":CORP_ID,"isactive":ISACTIVE};
-				interfacejs.ajaxSubmit(_command,_params,opt);
+				var _params={"id":ID,"user_code":USER_CODE,"feedback_date":FEEDBACK_DATE,"phone":PHONE,
+				"feedback_content":FEEDBACK_CONTENT,"process_state":PROCESS_STATE,"isactive":ISACTIVE};
+				feedbackjs.ajaxSubmit(_command,_params,opt);
 			}else{
 				return;
 			}
 		});
 	};
-	interfacejs.ajaxSubmit=function(_command,_params,opt){
+	feedbackjs.ajaxSubmit=function(_command,_params,opt){
 		// console.log(JSON.stringify(_params));
 		// _params=JSON.stringify(_params);
 		console.log(_params);
@@ -95,7 +103,7 @@ var oc = new ObjectControl();
 				// 	cancel: false,
 				// 	content: data.message
 				// });
-				$(window.parent.document).find('#iframepage').attr("src","/system/interface.html");
+				$(window.parent.document).find('#iframepage').attr("src","/system/feedback.html");
 			}else if(data.code=="-1"){
 				// alert(data.message);
 				// art.dialog({
@@ -117,8 +125,8 @@ var oc = new ObjectControl();
 		var command = _this.attr("verify");
 		var obj = _this.val();
 		var hint = _this.nextAll(".hint").children();
-		if(interfacejs['check' + command]){
-			if(!interfacejs['check' + command].apply(interfacejs,[obj,hint])){
+		if(feedbackjs['check' + command]){
+			if(!feedbackjs['check' + command].apply(feedbackjs,[obj,hint])){
 				return false;
 			}
 		}
@@ -133,33 +141,30 @@ var oc = new ObjectControl();
 		clearInterval(interval);
 	});
 	var init=function(){
-		interfacejs.bindbutton();
+		feedbackjs.bindbutton();
 	}
 	var obj = {};
-	obj.interfacejs = interfacejs;
+	obj.feedbackjs = feedbackjs;
 	obj.init = init;
 	return obj;
 }));
 
 jQuery(document).ready(function(){
-	window.interface.init();//初始化
-	if($(".pre_title label").text()=="编辑接口管理"){
+	window.feedback.init();//初始化
+	if($(".pre_title label").text()=="编辑用户反馈"){
 		var id=sessionStorage.getItem("id");
 		var _params={"id":id};
-		var _command="/interfacers/selectById";
+		var _command="/feedback/selectById";
 		oc.postRequire("post", _command,"", _params, function(data){
 			console.log(data);
 			if(data.code=="0"){
 				var msg=JSON.parse(data.message);
-				// msg=JSON.parse(msg.appversion);
 				console.log(msg);
-				$('#INTERFACE_VERSION').val(msg.version);
-				$("#CORP_ID").val(msg.corp_code);
-
-				$("#created_time").val(msg.created_date);
-				$("#creator").val(msg.creater);
-				$("#modify_time").val(msg.modified_date);
-				$("#modifier").val(msg.modifier);
+				$('#USER_CODE').val(msg.user_code);
+				$("#FEEDBACK_DATE").val(msg.feedback_date);
+				$("#PHONE").val(msg.phone);
+				$("#FEEDBACK_CONTENT").val(msg.feedback_content);
+				$("#PROCESS_STATE").val(msg.process_state);
 				var input=$(".checkbox_isactive").find("input")[0];
 				if(msg.isactive=="Y"){
 					input.checked=true;
@@ -182,9 +187,9 @@ jQuery(document).ready(function(){
 		console.log(123);
 	})
 	$(".operadd_btn ul li:nth-of-type(2)").click(function(){
-		$(window.parent.document).find('#iframepage').attr("src","/system/interface.html");
+		$(window.parent.document).find('#iframepage').attr("src","/system/feedback.html");
 	});
 	$(".operedit_btn ul li:nth-of-type(2)").click(function(){
-		$(window.parent.document).find('#iframepage').attr("src","/system/interface.html");
+		$(window.parent.document).find('#iframepage').attr("src","/system/feedback.html");
 	});
 });
