d97e70ea23d4f1c622981e9606ebe1b84df896dd
liuying8133 <liuying8133@163.com>
2016-06-30 14:30:39 +0800

    add

---
+++ iShop/src/main/java/com/bizvane/ishop/service/TableManagerService.java
@@ -0,0 +1,12 @@
+package com.bizvane.ishop.service;
+
+import com.bizvane.ishop.entity.TableManager;
+
+import java.util.List;
+
+/**
+ * Created by yin on 2016/6/30.
+ */
+public interface TableManagerService {
+    List<TableManager> selAllByCode(String function_code);
+}
