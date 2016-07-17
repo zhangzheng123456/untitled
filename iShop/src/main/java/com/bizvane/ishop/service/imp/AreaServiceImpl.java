b5497f8c0a718f09953501924091799ce090b4f5
lixixi89055465 <1850094299@qq.com>
2016-07-17 21:56:52 +0800

    区域管理筛选

--- iShop/src/main/java/com/bizvane/ishop/service/imp/AreaServiceImpl.java
+++ iShop/src/main/java/com/bizvane/ishop/service/imp/AreaServiceImpl.java
@@ -15,7 +15,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ZhouZhou on 2016/6/4.
@@ -74,14 +76,14 @@ public class AreaServiceImpl implements AreaService {
 
     @Override
     @Transactional
-    public String insert(String message,String user_id) throws SQLException {
+    public String insert(String message, String user_id) throws SQLException {
         String result = Common.DATABEAN_CODE_ERROR;
         JSONObject jsonObject = new JSONObject(message);
         String area_code = jsonObject.get("area_code").toString();
         String corp_code = jsonObject.get("corp_code").toString();
         String area_name = jsonObject.get("area_name").toString();
-        Area area = getAreaByCode(corp_code,area_code);
-        Area area1 = getAreaByName(corp_code,area_name);
+        Area area = getAreaByCode(corp_code, area_code);
+        Area area1 = getAreaByName(corp_code, area_name);
         if (area == null && area1 == null) {
             area = new Area();
             Date now = new Date();
@@ -95,9 +97,9 @@ public class AreaServiceImpl implements AreaService {
             area.setIsactive(jsonObject.get("isactive").toString());
             areaMapper.insertArea(area);
             result = Common.DATABEAN_CODE_SUCCESS;
-        }else if(area != null){
+        } else if (area != null) {
             result = "区域编号已存在";
-        }else {
+        } else {
             result = "区域名称已存在";
         }
         return result;
@@ -105,7 +107,7 @@ public class AreaServiceImpl implements AreaService {
 
     @Override
     @Transactional
-    public String update(String message,String user_id) throws SQLException {
+    public String update(String message, String user_id) throws SQLException {
         String result = Common.DATABEAN_CODE_ERROR;
         JSONObject jsonObject = new JSONObject(message);
         int area_id = Integer.parseInt(jsonObject.get("id").toString());
@@ -115,8 +117,8 @@ public class AreaServiceImpl implements AreaService {
         String area_name = jsonObject.get("area_name").toString();
 
         Area area = getAreaById(area_id);
-        Area area1 = getAreaByCode(corp_code,area_code);
-        Area area2 = getAreaByName(corp_code,area_code);
+        Area area1 = getAreaByCode(corp_code, area_code);
+        Area area2 = getAreaByName(corp_code, area_code);
 
         if ((area.getArea_code().equals(area_code) || area1 == null)
                 && (area.getArea_name().equals(area_name) || area2 == null)) {
@@ -131,9 +133,9 @@ public class AreaServiceImpl implements AreaService {
             area.setIsactive(jsonObject.get("isactive").toString());
             areaMapper.updateArea(area);
             result = Common.DATABEAN_CODE_SUCCESS;
-        }else if (!area.getArea_code().equals(area_code) && area1 != null){
+        } else if (!area.getArea_code().equals(area_code) && area1 != null) {
             result = "区域编号已存在";
-        }else {
+        } else {
             result = "区域名称已存在";
         }
         return result;
@@ -159,12 +161,24 @@ public class AreaServiceImpl implements AreaService {
 
     @Override
     public Area selAreaByCorp(String corp_code, String area_code, String isactive) {
-        return areaMapper.selAreaByCorp(corp_code,area_code,isactive);
+        return areaMapper.selAreaByCorp(corp_code, area_code, isactive);
     }
 
     @Override
     public String insertExecl(Area area) {
         areaMapper.insertArea(area);
         return "add success";
+    }
+
+    @Override
+    public PageInfo<Area> getAllAreaScreen(int page_number, int page_size, String corp_code, Map<String, String> map) {
+        List<Area> areas;
+        PageHelper.startPage(page_number, page_size);
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("corp_code", corp_code);
+        params.put("map", map);
+        areas = areaMapper.selectAllAreaScreen(params);
+        PageInfo<Area> page = new PageInfo<Area>();
+        return page;
     }
 }
