f76f5e19e4209468955e3c87e568434d3896d717
liuying8133 <liuying8133@163.com>
2016-07-27 11:01:35 +0800

    ss

--- iShop/src/main/java/com/bizvane/ishop/dao/TaskMapper.java
+++ iShop/src/main/java/com/bizvane/ishop/dao/TaskMapper.java
@@ -1,8 +1,16 @@
 package com.bizvane.ishop.dao;
 
+import com.bizvane.ishop.entity.Task;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * Created by yin on 2016/7/21.
  */
 public interface TaskMapper {
+        List<Task> selectAllTask(@Param("corp_code")String corp_code,@Param("role_ident")String role_ident,@Param("user_code")String user_code,@Param("search_value")String search_value);
+
+
 
 }
