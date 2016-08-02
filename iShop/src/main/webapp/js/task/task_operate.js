a585fa9e513a0d48d1ff9c77dc53356dba7141bf
lishun <773538995@qq.com>
2016-08-02 15:43:20 +0800

    修改

---
+++ iShop/src/main/webapp/js/task/task_operate.js
@@ -0,0 +1,48 @@
+var oc = new ObjectControl();
+var a="";
+var b="";
+function getasktypelist(a,b){
+	var corp_command="/task/selectAllTaskType";
+	var _param={};
+	_param["corp_code"]=a;
+	oc.postRequire("post",corp_command,"0",_param,function(data){
+		console.log(data);
+	})
+}
+function getcorplist(a,b){
+	//获取所属企业列表
+	var corp_command="/user/getCorpByUser";
+	oc.postRequire("post", corp_command,"", "", function(data){
+		console.log(data);
+		if(data.code=="0"){
+			var msg=JSON.parse(data.message);
+			console.log(msg);
+			var index=0;
+			var corp_html='';
+			var c=null;
+			for(index in msg.corps){
+				c=msg.corps[index];
+				corp_html+='<option value="'+c.corp_code+'">'+c.corp_name+'</option>';
+			}
+			$("#OWN_CORP").append(corp_html);
+			if(a!==""){
+				$("#OWN_CORP option[value='"+a+"']").attr("selected","true");
+			}
+			$("#OWN_CORP").searchableSelect();
+			var c=$('#corp_select .selected').attr("data-value");
+			getasktypelist(c,b);
+			$("#corp_select .searchable-select-item").click(function(){
+				var c=$(this).attr("data-value");
+				getasktypelist(c,b);
+			})
+		}else if(data.code=="-1"){
+			art.dialog({
+				time: 1,
+				lock:true,
+				cancel: false,
+				content: data.message
+			});
+		}
+	});
+}
+getcorplist(a,b);
