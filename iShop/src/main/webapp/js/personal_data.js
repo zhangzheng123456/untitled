77eea4e41ec7aed2037be06202d80745f165ce57
ghy <312169513@qq.com>
2016-08-09 16:02:49 +0800

    bug

--- iShop/src/main/webapp/js/personal_data.js
+++ iShop/src/main/webapp/js/personal_data.js
@@ -1,7 +1,6 @@
 var oc = new ObjectControl();
 
  jQuery(document).ready(function(){
-     window.user.init();//初始化
      if($(".pre_title label").text()=="个人资料"){
          var id=sessionStorage.getItem("id");
          var _params={"id":id};
@@ -10,6 +9,8 @@ var oc = new ObjectControl();
              console.log(data);
              if(data.code=="0"){
                  var msg = JSON.parse(data.message);
+                     msg=JSON.parse(msg.user);
+                 console.log(msg);
                  $("#IMG").val(msg.user_code);
                  $("#OWN_CORP").val(msg.corp_name);
                  $("#USER_ID").val(msg.user_code);
@@ -18,7 +19,11 @@ var oc = new ObjectControl();
                  $("#IPHONE").val(msg.phone);
                  $("#USER_EMAIL").val(msg.email);
                  $("#PASSWORD").val(msg.password);
-                 $("#USER_SEX").val(msg.sex);
+                 if(msg.sex=="F"){
+                     $("#USER_SEX").val("女");
+                 }else if(msg.sex=="M"){
+                     $("#USER_SEX").val("男");
+                 }
                  $("#OWN_GROUP").val(msg.group_name);
                  $("#OWN_SHOP").val(msg.area_name);
              }
