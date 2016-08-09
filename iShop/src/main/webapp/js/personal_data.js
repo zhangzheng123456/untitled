bef2184f7b33afa61957cb4a2a1490bbb95251db
ghy <312169513@qq.com>
2016-08-09 21:14:27 +0800

    个人资料

--- iShop/src/main/webapp/js/personal_data.js
+++ iShop/src/main/webapp/js/personal_data.js
@@ -30,3 +30,78 @@ var oc = new ObjectControl();
          })
      }
 })
+//性别选择下拉框显示/隐藏
+    $("#USER_SEX").click(function(){
+        if($("#sex_down").css("display")=="none"){
+            $("#sex_down").show();
+        }else if($("#sex_down").css("display")=="block"){
+            $("#sex_down").hide();
+        }
+    });
+
+    $("#sex_down li").click(function(){
+     var sex=$(this).text();
+     $("#USER_SEX").val(sex);
+     $("#sex_down").hide();
+})
+//重置密码时提示消失
+$("#first_pwd").focus(function () {
+    $(".em_1").css("display","none")
+})
+$("#second_pwd").focus(function () {
+    $(".em_2").css("display","none")
+})
+//重置密码
+$("#baocun").click(function(){
+    if($("#first_pwd").val()==""||$("#second_pwd").val()==""||$("#first_pwd").val()!=$("#second_pwd").val()){
+        if($("#first_pwd").val()==""){
+            $(".em_1").css("display","block")
+        }else if($("#second_pwd").val()!=$("#first_pwd").val()){
+            $(".em_2").css("display","block")
+        }
+        return;
+    }
+    var pwd_creat="/user/change_passwd";
+    var user_id=sessionStorage.getItem("id");
+    var password=$('#first_pwd').val();
+    var _params={};
+    _params["password"]=password;
+    _params["user_id"]=user_id;
+    oc.postRequire("post",pwd_creat,"",_params,function (data) {
+        if(data.code=="0"){
+            art.dialog({
+                time: 1,
+                lock:true,
+                cancel: false,
+                content: data.message
+            });
+            $("#chongzhi_pwd").css('display','none');
+            $("#chongzhi_box").css('display','none');
+        }else if(data=="-1"){
+            art.dialog({
+                time: 1,
+                lock:true,
+                cancel: false,
+                content: 失败
+            });
+        }
+
+    })
+})
+
+//关闭
+$("#personal_close").click(function () {
+    $(window.parent.document).find('#iframepage').attr("src","http://ishop.dev.bizvane.com/");
+})
+function chongzhi() {
+    $("#chongzhi_pwd").css('display','block');
+    $("#chongzhi_box").css('display','block');
+}
+function quxiao() {
+    $("#first_pwd").val("");
+    $("#second_pwd").val("");
+    $("#chongzhi_pwd").css('display','none');
+    $("#chongzhi_box").css('display','none');
+    $(".em_1").css("display","none");
+    $(".em_2").css("display","none")
+}
