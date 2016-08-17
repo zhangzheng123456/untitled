7d60f0403a2d26427b2a88f5f8d9dd3fc3c9d349
ghy <312169513@qq.com>
2016-08-17 13:49:11 +0800

    fab最终版

--- iShop/src/main/webapp/goods/mobile/js/recommend.js
+++ iShop/src/main/webapp/goods/mobile/js/recommend.js
@@ -1,152 +1,95 @@
 jQuery(function(){
 	var url = location.search; //获取url中"?"符后的字串   
-        if (url.indexOf("?") != -1) {   
-	        var str = url.substr(1);   
-	        strs = str.split("=")[1];
-		}
-
-	var hasNextPage="";
-	var is
+	if (url.indexOf("?") != -1) {
+		var str = url.substr(1);
+		strs = str.split("=")[1];
+	}
 	var corp_code=strs;
 	console.log(corp_code);
 	var oc = new ObjectControl();
 	var rowno=0;
-	var key;
-	var param = {
-        "rowno":rowno,
-        "corp_code":corp_code,
-        "key":key
-	};
-	if(corp_code!=""){
-		oc.postRequire("post","/api/fab","0",param,function(data){
-			console.log(data);
-			var list=JSON.parse(data.message);
-			var list=JSON.parse(list.list);
-			counts=list.pages;
-			hasNextPage=list.hasNextPage;
-			var list=list.list;
-			console.log(list);
-			for(var i=0;i<list.length;i++){
-				jQuery('.allShops').append('<div class="shop"><a href="goods.html?corp_code='+corp_code+'&id='+list[i].id+'"><div class="img"><img src="'+list[i].goods_image+'"></div><div class="shop-t"><h1>'+list[i].goods_name+'</h1><p>货号:'+list[i].goods_code+'</p><p class="pice">价格:<span>￥'+list[i].goods_price+'</span></p></div></a></div>');
-			}
-			morehide();
-			morelist();
-		});
-
-		function searCh(){
-	        query = {
-		        "rowno":rowno,
-		        "corp_code":corp_code,
-		        "search_value":key
-		    };
-		    console.log(rowno);
-		    oc.postRequire("post","/api/fab/search","0",query,function(data){
-		    	console.log(data);
-				var list=JSON.parse(data.message);
-				var list=JSON.parse(list.list);
-				hasNextPage=list.hasNextPage;
-				var list=list.list;
-		    	console.log(list);
-		    	if(list.length>=1){
-		    		jQuery('.allShops').empty();
-		    		for(var i=0;i<list.length;i++){
-				    		jQuery('.allShops').append('<div class="shop"><a href="goods.html?corp_code='+corp_code+'&id='+list[i].id+'"><div class="img"><img src="'+list[i].goods_image+'"></div><div class="shop-t"><h1>'+list[i].goods_name+'</h1><p>货号:'+list[i].goods_code+'</p><p class="pice">价格:<span>￥'+list[i].goods_price+'</span></p></div></a></div>');
-			}
-					morehide();
-					if(hasNextPage==true){
-							$(".more").show();
-						    $(".more").click(function() {
-							var rowno = jQuery('.allShops .shop').length+1;
-							param = {
-								"rowno": rowno,
-								"corp_code": corp_code,
-								"search_value": key
-							}
-							oc.postRequire("post", "/api/fab/search", "0", param, function (data) {
-								console.log(data);
-								var list = JSON.parse(data.message);
-								var list = JSON.parse(list.list);
-								hasNextPage=list.hasNextPage;
-								var list = list.list;
-								for (var i = 0; i < list.length; i++) {
-									jQuery('.allShops').append('<div class="shop"><a href="goods.html?corp_code=' + corp_code + '&goods_price=' + list[i].goods_code + '"><div class="img"><img src="' + list[i].goods_image + '"></div><div class="shop-t"><h1>' + list[i].goods_name + '</h1><p>货号:' + list[i].goods_code + '</p><p class="pice">价格:<span>￥' + list[i].goods_price + '</span></p></div></a></div>');
-								}
-								morehide();
-							})
-						})
-
-					}
-		    	}else if(list.length==0){
-		    		jQuery('.allShops').empty();
-		    		jQuery(".allShops").append("<p>没有收索结果</p><p class='p'>没有找到相关宝贝<p/>")
-					morehide();
-		    	}else if(key==""){
-		    		jQuery('.allShops').empty();
-					jQuery('.allShops').append('<div class="shop"><a href="goods.html?corp_code='+corp_code+'&id='+list[i].id+'"><div class="img"><img src="'+list[i].goods_image+'"></div><div class="shop-t"><h1>'+list[i].goods_name+'</h1><p>货号:'+list[i].goods_code+'</p><p class="pice">价格:<span>￥'+list[i].goods_price+'</span></p></div></a></div>');
-					morehide();
-					morelist();
-				}
-		    });
-		  console.log(key); 
+	var value="";
+	//页面加载循环
+	function superaddition(list){
+		for (var i = 0; i < list.length; i++) {
+			jQuery('.allShops').append('<div class="shop"><a href="goods.html?corp_code='
+				+ corp_code
+				+ '&id='
+				+ list[i].id
+				+ '"><div class="img"><img src="'
+				+ list[i].goods_image
+				+ '"></div><div class="shop-t"><h1>'
+				+ list[i].goods_name + '</h1><p>货号:'
+				+ list[i].goods_code + '</p><p class="pice">价格:<span>￥'
+				+ list[i].goods_price + '</span></p></div></a></div>');
 		}
-		jQuery(document).keydown(function(event){
-			var event=window.event||arguments[0];
-			var code=event.keyCode;
-			if(code==13){
-	            key=$('#input').val().replace(/\s+/g,"");
-	            if(key!=""){
-				searCh();}}
-		})
-        // $('#input').bind('input propertychange', function() {
-			// key=$('#input').val().replace(/\s+/g,"");
-			// console.log(key);
-			// 	searCh();
-	    // });
-
-		$("#d_search").click(function () {
-			key=$('#input').val().replace(/\s+/g,"");
-			searCh();
-		})
-
-		function morehide() {
-			var shop_len=$(".shop").length;
-			console.log(hasNextPage);
+	}
+	//刚进页面的list请求
+	function getList(a){
+		var param={};
+		param["rowno"]=a;
+		param["corp_code"]=corp_code;
+		oc.postRequire("post","/api/fab/","0",param,function(data){
+			console.log(data);
+			var message=JSON.parse(data.message);
+			var list=JSON.parse(message.list);
+			var hasNextPage=list.hasNextPage;
+			cout=list.pages;
+			var list=list.list;
+			superaddition(list);
+			if(hasNextPage==true){
+				$(".more").show();
+			}
 			if(hasNextPage==false){
 				$(".more").hide();
 			}
-		}
-
-		function morelist() {
+		})
+	}
+	//input输入框里面
+	$('#input').bind('input propertychange', function() {
+		value=$('#input').val().replace(/\s+/g,"");
+		getSearchList(rowno);
+	});
+	//搜索加载list
+	function getSearchList(a){
+		var param={};
+		param["rowno"]=a;
+		param["corp_code"]=corp_code;
+		param["search_value"]=value;
+		oc.postRequire("post","/api/fab/search","0",param,function(data){
+			var message=JSON.parse(data.message);
+			var list=JSON.parse(message.list);
+			var hasNextPage=list.hasNextPage;
+			cout=list.pages;
+			var list=list.list;
 			if(hasNextPage==true){
 				$(".more").show();
-				$(".more").click(function() {
-				  if($("#input").val()=="") {
-					  var rowno = jQuery('.allShops .shop').length + 1;
-					  query = {
-						  "rowno": rowno,
-						  "corp_code": corp_code,
-						  "key": key
-					  }
-					  console.log(rowno);
-					  console.log(key);
-					  oc.postRequire("post", "/api/fab", "", query, function (data) {
-						  console.log(data);
-						  var list = JSON.parse(data.message);
-						  var list = JSON.parse(list.list);
-						  counts = list.pages;
-						  hasNextPage = list.hasNextPage;
-						  var list = list.list;
-						  for (var i = 0; i < list.length; i++) {
-							  jQuery('.allShops').append('<div class="shop"><a href="goods.html?corp_code=' + corp_code + '&goods_price=' + list[i].goods_code + '"><div class="img"><img src="' + list[i].goods_image + '"></div><div class="shop-t"><h1>' + list[i].goods_name + '</h1><p>货号:' + list[i].goods_code + '</p><p class="pice">价格:<span>￥' + list[i].goods_price + '</span></p></div></a></div>');
-						  }
-						  morehide();
-					  })
-				  }
-				})
-
 			}
+			if(hasNextPage==false){
+				$(".more").hide();
+			}
+			if(list.length<0){
+				jQuery('.allShops').empty();
+				jQuery(".allShops").append("<p>没有收索结果</p><p class='p'>没有找到相关宝贝<p/>")
+			}
+			if(a<20){
+				jQuery('.allShops').empty();
+			}
+			if(list.length>0){
+				superaddition(list);
+			}
+		})
+	}
+	getList(rowno);
+	//点击加载更多
+	$(".more").click(function(){
+		var rowno=$('.allShops .shop').length;
+		if(value==""){
+			getList(rowno);
+		}else if(value!==""){
+			getSearchList(rowno);
 		}
-    }
 
+	})
 });
 
