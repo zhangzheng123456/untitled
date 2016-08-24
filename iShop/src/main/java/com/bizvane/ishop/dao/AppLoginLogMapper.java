2305f7434d53899bbbef35f482ef5859d4bec927
liuying8133 <liuying8133@163.com>
2016-08-24 16:59:24 +0800

    业绩

--- iShop/src/main/java/com/bizvane/ishop/dao/AppLoginLogMapper.java
+++ iShop/src/main/java/com/bizvane/ishop/dao/AppLoginLogMapper.java
@@ -1,7 +1,14 @@
 package com.bizvane.ishop.dao;
 
+import com.bizvane.ishop.entity.AppLoginLog;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * Created by yin on 2016/8/23.
  */
 public interface AppLoginLogMapper {
+    List<AppLoginLog> selectAllAppLoginLog(@Param("corp_code")String corp_code, @Param("search_value")String search_value);
+
 }
