e353e92da4c24ecf9dc44d6b9207ec44ef219cf1
ghy <312169513@qq.com>
2016-08-30 13:40:36 +0800

    参数

--- iShop/src/main/webapp/js/system/errorlog.js
+++ iShop/src/main/webapp/js/system/errorlog.js
@@ -729,3 +729,21 @@ $("#input-txt").keydown(function() {
         };
     }
 })
+
+$(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()>100){
+            $("#side_bar .gotop").fadeIn();
+        }
+        else{
+            $("#side-bar .gotop").hide();
+        }
+    });
+    $("#side_bar .gotop").click(function(){
+        $('html,body').animate({'scrollTop':0},500);
+    });
+    $("#side_bar .gobottom").click(function(){
+        var btm=$('html,body').height();
+        $('html,body').animate({'scrollTop':btm},500);
+    })
+});
