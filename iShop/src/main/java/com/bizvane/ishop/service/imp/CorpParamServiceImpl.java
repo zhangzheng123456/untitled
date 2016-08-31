080fbc830b979272c239271f1c9ae4b6f99276da
jiangyan <1042034123@qq.com>
2016-08-31 11:24:52 +0800

    bug-企业参数

--- iShop/src/main/java/com/bizvane/ishop/service/imp/CorpParamServiceImpl.java
+++ iShop/src/main/java/com/bizvane/ishop/service/imp/CorpParamServiceImpl.java
@@ -100,7 +100,6 @@ public class CorpParamServiceImpl implements CorpParamService {
         String remark = jsonObject.get("remark").toString();
         String corp_code = jsonObject.get("corp_code").toString();
         String param_id = jsonObject.get("param_id").toString();
-        String param_name = jsonObject.get("param_name").toString();
 
         String param_value = jsonObject.get("param_value").toString();
         List<CorpParam> corpParams = selectByCorpParam(corp_code, param_id);
@@ -110,7 +109,6 @@ public class CorpParamServiceImpl implements CorpParamService {
             Date now = new Date();
             corpParam.setId(id);
             corpParam.setRemark(remark);
-            corpParam.setParam_name(param_name);
             corpParam.setParam_id(param_id);
             corpParam.setParam_value(param_value);
             corpParam.setCorp_code(corp_code);
