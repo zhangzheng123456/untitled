cef71088f649fe375a0c7d84fae5569ea5abe35c
chtty <6314860042@qq.com>
2016-09-01 09:12:36 +0800

    导出模块样式修改

--- iShop/src/main/webapp/js/task/task_type.js
+++ iShop/src/main/webapp/js/task/task_type.js
@@ -540,21 +540,34 @@ $("#leading_out").click(function(){
             var message=JSON.parse(data.message);
             var message=JSON.parse(message.tableManagers);
             console.log(message);
-            $("#file_list ul").empty();
+            $("#file_list_l ul").empty();
             for(var i=0;i<=message.length;i++){
-                 $("#file_list ul").append("<li data-name='"+message[i].column_name+"'><div class='checkbox1'><input type='checkbox' value='' name='test'  class='check'  id='checkboxInput"
+                 $("#file_list_l ul").append("<li data-name='"+message[i].column_name+"'><div class='checkbox1'><input type='checkbox' value='' name='test'  class='check'  id='checkboxInput"
                 +i+1+"'/><label for='checkboxInput"+i+1+"'></label></div><span class='p15'>"+message[i].show_name+"</span></li>")
             }
+            bianse();
+            $("#file_list_r ul").empty();
         }else if(data.code=="-1"){
             alert(data.message);
         }
     })
 })
+function bianse(){
+    $("#file_list_l li:odd").css("backgroundColor","#fff");
+    $("#file_list_l li:even").css("backgroundColor","#ededed");
+    $("#file_list_r li:odd").css("backgroundColor","#fff");
+    $("#file_list_r li:even").css("backgroundColor","#ededed");
+}
 //导出提交的
 $("#file_submit").click(function(){
-    var li=$("#file_list input[type='checkbox']:checked").parents("li");
+    var li=$("#file_list_r input[type='checkbox']:checked").parents("li");
     var param={};
     var tablemanager=[];
+    if(li.length=="0"){
+        frame();
+        $('.frame').html('请把要导出的列移到右边');
+        return;
+    }
     for(var i=0;i<li.length;i++){
         var r=$(li[i]).attr("data-name");
         var z=$(li[i]).children("span").html();
