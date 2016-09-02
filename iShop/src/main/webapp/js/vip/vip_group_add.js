7550684b2a74da2a714614a441d32e3a6db1e1bf
zhaowei <1174952035@qq.com>
2016-09-02 11:52:54 +0800

    会员分组编辑

--- iShop/src/main/webapp/js/vip/vip_group_add.js
+++ iShop/src/main/webapp/js/vip/vip_group_add.js
@@ -2,7 +2,60 @@ var oc = new ObjectControl();
 $(function(){
     window.vip.init();
     if($(".pre_title label").text()=="编辑会员分组"){
-        console.log("这是编辑界面")
+            var id=sessionStorage.getItem("id");
+            var key_val=sessionStorage.getItem("key_val");//取页面的function_code
+            key_val=JSON.parse(key_val);
+            var funcCode=key_val.func_code;
+            whir.loading.add("",0.5);
+            $.get("/detail?funcCode="+funcCode+"", function(data){
+                var data=JSON.parse(data);
+                if(data.code=="0"){
+                    var message=JSON.parse(data.message);
+                    var action=message.actions;
+                    if(action.length<=0){
+                        $("#edit_save").remove();
+                    }
+                }
+            });
+            var _params={};
+            _params["id"]=id;
+            var _command="/vipGroup/select";
+            oc.postRequire("post", _command,"", _params, function(data){
+                console.log(data);
+                if(data.code=="0"){
+                    var msg=JSON.parse(data.message);
+                    console.log(msg);
+                    $("#vip_id").val(msg.vip_group_name);
+                    $("#vip_id").attr("data-name",msg.vip_group_name);
+                    $("#vip_num").val(msg.vip_group_code);
+                    $("#vip_num").attr("data-name",msg.vip_group_code);
+                    $("#vip_remark").val(msg.remark);
+                    $("#OWN_CORP option").val(msg.corp.corp_code);
+                    console.log(msg.corp.corp_code);
+                    $("#OWN_CORP option").text(msg.corp.corp_name);
+                    //$("#area_shop").val("共"+msg.store_count+"家店铺");
+                    // $("#OWN_CORP").val(msg.corp_code);
+                    $("#created_time").val(msg.created_date);
+                    $("#creator").val(msg.creater);
+                    $("#modify_time").val(msg.modified_date);
+                    $("#modifier").val(msg.modifier);
+                    var input=$(".checkbox_isactive").find("input")[0];
+                    if(msg.isactive=="Y"){
+                        input.checked=true;
+                    }else if(msg.isactive=="N"){
+                        input.checked=false;
+                    }
+                    getcorplist();
+                }else if(data.code=="-1"){
+                    art.dialog({
+                        time: 1,
+                        lock:true,
+                        cancel: false,
+                        content: data.message
+                    });
+                }
+                whir.loading.remove();//移除加载框
+            });
     }else{
         console.log("这是新增界面");
         getcorplist();
@@ -46,24 +99,22 @@ $(function(){
     };
     vipjs.bindbutton=function(){
         $(".areaadd_oper_btn ul li:nth-of-type(1)").click(function(){
-            //var codeMark=$("#vip_id").attr("data-mark");//区域名称是否唯一的标志
-            var nmae=$("#vip_id").attr("data-mark");//区域名称是否唯一的标志
-            //var nameMark=$("#vip_nym").attr("data-mark");//区域编号是否唯一的标志
+            var name=$("#vip_id").attr("data-mark");//区域名称是否唯一的标志
             var num=$("#vip_num").attr("data-mark");//区域编号是否唯一的标志
             if(vipjs.firstStep()){
-                //if(nameMark=="N"||codeMark=="N"){
-                //    if(nameMark=="N"){
-                //        var div=$("#AREA_NAME").next('.hint').children();
-                //        div.html("该名称已经存在！");
-                //        div.addClass("error_tips");
-                //    }
-                //    if(codeMark=="N"){
-                //        var div=$("#AREA_ID").next('.hint').children();
-                //        div.html("该编号已经存在！");
-                //        div.addClass("error_tips");
-                //    }
-                //    return;
-                //}
+                if(name=="N"||num=="N"){
+                    if(name=="N"){
+                        var div=$("#vip_id").next('.hint').children();
+                        div.html("该名称已经存在！");
+                        div.addClass("error_tips");
+                    }
+                    if(num=="N"){
+                        var div=$("#vip_num").next('.hint').children();
+                        div.html("该编号已经存在！");
+                        div.addClass("error_tips");
+                    }
+                    return;
+                }
                 var vip_id=$("#vip_id").val();
                 var vip_num=$("#vip_num").val();
                 var OWN_CORP=$("#OWN_CORP").val();
@@ -78,10 +129,9 @@ $(function(){
                 var _command="/vipGroup/add";//接口名
                 var opt = {//返回成功后的操作
                     success:function(){
-
                     }
                 };
-                var _params={"vip_group_code":vip_num,"vip_group_name":vip_id,"corp_name":OWN_CORP,"remark":vip_remark,"isactive":ISACTIVE};
+                var _params={"vip_group_code":vip_num,"vip_group_name":vip_id,"corp_code":OWN_CORP,"remark":vip_remark,"isactive":ISACTIVE};
                 vipjs.ajaxSubmit(_command,_params,opt);
             }else{
                 return;
@@ -130,7 +180,7 @@ $(function(){
     vipjs.ajaxSubmit=function(_command,_params,opt){
         oc.postRequire("post", _command,"",_params, function(data){
             if(data.code=="0"){
-                $(window.parent.document).find('#iframepage').attr("src","/area/area.html");
+                $(window.parent.document).find('#iframepage').attr("src","/vip/vip_group.html");
             }else if(data.code=="-1"){
                 art.dialog({
                     time: 1,
