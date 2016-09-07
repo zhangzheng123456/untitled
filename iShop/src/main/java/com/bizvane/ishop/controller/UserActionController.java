d492366332ea666b86dc643be8dcaf0c8a4b754c
zhou <781174667@qq.com>
2016-09-07 20:56:18 +0800

    用户行为日志

--- iShop/src/main/java/com/bizvane/ishop/controller/UserActionController.java
+++ iShop/src/main/java/com/bizvane/ishop/controller/UserActionController.java
@@ -15,10 +15,7 @@ import com.bizvane.sun.common.service.mongodb.MongoDBClient;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.PageInfo;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.QueryOperators;
+import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
@@ -28,6 +25,7 @@ import jxl.Workbook;
 import org.apache.log4j.Logger;
 import org.bson.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
@@ -71,51 +69,58 @@ public class UserActionController {
     @ResponseBody
     public String userActionList(HttpServletRequest request) {
         DataBean dataBean = new DataBean();
+        JSONObject result = new JSONObject();
+        int pages=0;
         try {
             String role_code = request.getSession(false).getAttribute("role_code").toString();
             String corp_code = request.getSession(false).getAttribute("corp_code").toString();
-//            int page_number = Integer.parseInt(request.getParameter("pageNumber"));
-//            int page_size = Integer.parseInt(request.getParameter("pageSize"));
-            MongoClient client = mongodbClient.getMongoClient();
-            MongoDatabase database = client.getDatabase("ishow");
-            MongoCollection<Document> collection = database.getCollection("vipActionLogger");
-            System.out.println("===========collection vipActionLogger: " + collection);
-            // 读取数据
-            MongoCursor<Document> cursor;
+            int page_number = Integer.parseInt(request.getParameter("pageNumber"));
+            int page_size = Integer.parseInt(request.getParameter("pageSize"));
+            System.out.println("======vipActionLogger===== ");
 
-//            if (page_number == 1) {
-//                if (role_code.equals(Common.ROLE_SYS)) {
-//                    cursor = collection.find().sort(sort).skip((page_number - 1) * page_size).limit(page_size).iterator();
-//                } else {
-//                    BsonDocument filter = new BsonDocument();
-//                    filter.append("CORP_CODE", new BsonString(corp_code));
-//                    cursor = collection.find(filter).sort(sort).skip((page_number - 1) * page_size).limit(page_size).iterator();
-//                }
-//            }else {
-//                String time = "";
-//                BasicDBObject queryObject = new BasicDBObject().append("time",new BasicDBObject().append(QueryOperators.GT,time));
-//                BsonDocument less = new BsonDocument();
-//                less.append("time",new BsonMinKey());
-//                if (role_code.equals(Common.ROLE_SYS)) {
-//                    cursor = collection.find().sort(sort).skip((page_number - 1) * page_size).limit(page_size).iterator();
-//                } else {
-//                    BsonDocument filter = new BsonDocument();
-//                    filter.append("CORP_CODE", new BsonString(corp_code));
-//                    cursor = collection.find(filter).sort(sort).skip((page_number - 1) * page_size).limit(page_size).iterator();
-//                }
-//            }
-            cursor = collection.find().skip(1).limit(3).iterator();
-            JSONArray array = new JSONArray();
-            while (cursor.hasNext()) {
-                System.out.print("------1111--cursor.hasNext()--------");
-                Document doc = cursor.next();
-                String list = doc.get("MSG").toString();
-//                String list = cursor.next().toString();
-                array.add(list);
+            MongoTemplate mongoTemplate = this.mongodbClient.getMongoTemplate();
+            DBCollection cursor = mongoTemplate.getCollection("log_person_action");
+
+            DBCursor dbCursor = null;
+            // 读取数据
+            if (role_code.equals(Common.ROLE_SYS)) {
+                int count = Integer.parseInt(String.valueOf(cursor.count()));
+                if (count%page_size == 0){
+                    pages = count/page_size;
+                }else {
+                    pages = count/page_size+1;
+                }
+                System.out.println("======vipActionLogger=====pages : " + pages);
+                dbCursor = cursor.find().skip((page_number - 1) * page_size).limit(page_size);
+                System.out.println("======sys=====dbCursor : " + dbCursor);
+            } else {
+                Map keyMap = new HashMap();
+                keyMap.put("corp_code",corp_code);
+                BasicDBObject ref = new BasicDBObject();
+                ref.putAll(keyMap);
+
+                DBCursor dbCursor1 = cursor.find(ref);
+                int count = Integer.parseInt(String.valueOf(dbCursor1.count()));
+                if (count%page_size == 0){
+                    pages = count/page_size;
+                }else {
+                    pages = count/page_size+1;
+                }
+                dbCursor = dbCursor1.skip((page_number - 1) * page_size).limit(page_size);
+                System.out.println("======other=====dbCursor : " + dbCursor);
             }
+            ArrayList list = new ArrayList();
+            while(dbCursor.hasNext()) {
+                DBObject obj = dbCursor.next();
+                list.add(obj.toMap());
+            }
+            result.put("list",list);
+            result.put("pages",pages);
+            result.put("page_number",page_number);
+            result.put("page_size",page_size);
             dataBean.setCode(Common.DATABEAN_CODE_SUCCESS);
             dataBean.setId("1");
-            dataBean.setMessage(JSON.toJSONString(array));
+            dataBean.setMessage(result.toString());
         } catch (Exception ex) {
             dataBean.setCode(Common.DATABEAN_CODE_ERROR);
             dataBean.setId("1");
@@ -170,9 +175,12 @@ public class UserActionController {
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     @ResponseBody
     public String search(HttpServletRequest request) {
-        String id = "";
         DataBean dataBean = new DataBean();
+        JSONObject result = new JSONObject();
+        int pages=0;
         try {
+            String role_code = request.getSession(false).getAttribute("role_code").toString();
+            String corp_code = request.getSession(false).getAttribute("corp_code").toString();
             String jsString = request.getParameter("param");
             org.json.JSONObject jsonObj = new org.json.JSONObject(jsString);
             id = jsonObj.get("id").toString();
@@ -181,20 +189,67 @@ public class UserActionController {
             int page_number = Integer.valueOf(jsonObject.get("pageNumber").toString());
             int page_size = Integer.valueOf(jsonObject.get("pageSize").toString());
             String search_value = jsonObject.get("searchValue").toString();
-            String role_code = request.getSession(false).getAttribute("role_code").toString();
-            JSONObject result = new JSONObject();
-            PageInfo<UserAchvGoal> list = null;
-            if (role_code.contains(Common.ROLE_SYS)) {
-                //系统管理员
+            System.out.println("======vipActionLogger===== ");
+
+            MongoTemplate mongoTemplate = this.mongodbClient.getMongoTemplate();
+            DBCollection cursor = mongoTemplate.getCollection("log_person_action");
+
+            BasicDBObject queryCondition = new BasicDBObject();
+            BasicDBList values = new BasicDBList();
+            values.add(new BasicDBObject("emp_id", search_value));
+            values.add(new BasicDBObject("url", search_value));
+            values.add(new BasicDBObject("corp_name", search_value));
+            values.add(new BasicDBObject("time", search_value));
+            values.add(new BasicDBObject("vip_id", search_value));
+            values.add(new BasicDBObject("action", search_value));
+            values.add(new BasicDBObject("emp_name", search_value));
+            values.add(new BasicDBObject("corp_code", search_value));
+            queryCondition.put("$or", values);
+
+            DBCursor dbCursor = null;
+            // 读取数据
+            if (role_code.equals(Common.ROLE_SYS)) {
+                DBCursor dbCursor1 = cursor.find(queryCondition);
+                int count = Integer.parseInt(String.valueOf(dbCursor1.count()));
+                if (count%page_size == 0){
+                    pages = count/page_size;
+                }else {
+                    pages = count/page_size+1;
+                }
+                System.out.println("======vipActionLogger=====pages : " + pages);
+                dbCursor = dbCursor1.skip((page_number - 1) * page_size).limit(page_size);
+                System.out.println("======sys=====dbCursor : " + dbCursor);
             } else {
+                Map keyMap = new HashMap();
+                keyMap.put("corp_code",corp_code);
+                BasicDBObject ref = new BasicDBObject();
+                ref.putAll(keyMap);
+
+                DBCursor dbCursor1 = cursor.find(ref,queryCondition);
+                int count = Integer.parseInt(String.valueOf(dbCursor1.count()));
+                if (count%page_size == 0){
+                    pages = count/page_size;
+                }else {
+                    pages = count/page_size+1;
+                }
+                dbCursor = dbCursor1.skip((page_number - 1) * page_size).limit(page_size);
+                System.out.println("======other=====dbCursor : " + dbCursor);
             }
-            result.put("list", JSON.toJSONString(list));
+            ArrayList list = new ArrayList();
+            while(dbCursor.hasNext()) {
+                DBObject obj = dbCursor.next();
+                list.add(obj.toMap());
+            }
+            result.put("list",list);
+            result.put("pages",pages);
+            result.put("page_number",page_number);
+            result.put("page_size",page_size);
             dataBean.setCode(Common.DATABEAN_CODE_SUCCESS);
-            dataBean.setId(id);
+            dataBean.setId("1");
             dataBean.setMessage(result.toString());
         } catch (Exception ex) {
             dataBean.setCode(Common.DATABEAN_CODE_ERROR);
-            dataBean.setId(id);
+            dataBean.setId("1");
             dataBean.setMessage(ex.getMessage());
             logger.info(ex.getMessage());
         }
