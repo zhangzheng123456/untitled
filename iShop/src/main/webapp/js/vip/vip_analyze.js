b85dc4793a73dec0646150e95a6ac93359447501
chtty <6314860042@qq.com>
2016-09-08 20:59:19 +0800

    会员分析

--- iShop/src/main/webapp/js/vip/vip_analyze.js
+++ iShop/src/main/webapp/js/vip/vip_analyze.js
@@ -1,4 +1,4 @@
-var oc = new ObjectControl();
+//var oc = new ObjectControl();
 var pageNumber=1;
 var pageSize=10;
 var searchValue='';
@@ -11,19 +11,23 @@ param['pageNumber']=pageNumber;
 param['pageSize']=pageSize;
 param['searchValue']=searchValue;
 console.log(param);
-oc.postRequire("post","/area/findAreaByCorpCode","",param,function(data){
-    console.log(data);
-    //if(data.code=="0"){
-    //    $(".table tbody").empty();
-    //    var message=JSON.parse(data.message);
-    //    console.log(message);
-    //    var list=message.all_vip_list;
-    //    cout=message.pages;
-    //    //var list=list.list;
-    //    superaddition(list,a);
-    //    jumpBianse();
-    //    setPage($("#foot-num")[0],cout,a,b,funcCode);
-    //}else if(data.code=="-1"){
-    //    alert(data.message);
-    //}
-});
+//oc.postRequire("post","/area/findAreaByCorpCode","",param,function(data){
+//    console.log(data);
+//    if(data.code=="0"){
+//        $(".table tbody").empty();
+//        var message=JSON.parse(data.message);
+//        console.log(message);
+//        var list=message.all_vip_list;
+//        cout=message.pages;
+//        //var list=list.list;
+//        superaddition(list,a);
+//        jumpBianse();
+//        setPage($("#foot-num")[0],cout,a,b,funcCode);
+//    }else if(data.code=="-1"){
+//        alert(data.message);
+//    }
+//});
+//œ‘ æselcet
+function show_select(){
+    $('#select_analyze').toggle();
+}
