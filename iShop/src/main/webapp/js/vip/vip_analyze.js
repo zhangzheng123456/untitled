3c2927d86ac5985e872c5194b2ce2db9eceec851
chtty <6314860042@qq.com>
2016-09-09 15:48:57 +0800

    会员分析

--- iShop/src/main/webapp/js/vip/vip_analyze.js
+++ iShop/src/main/webapp/js/vip/vip_analyze.js
@@ -1,49 +1,44 @@
 var oc = new ObjectControl();
 var pageNumber=1;
-var pageSize=10;
+var pageSize=7;
 var searchValue='';
-/*
- area/findAreaByCorpCode   post      pageNumber,pageSize,searchValue
-*/
 //加载区域
 function GetArea(){
     var param={};
     param['pageNumber']=pageNumber;
     param['pageSize']=pageSize;
     param['searchValue']=searchValue;
-    console.log(param);
     oc.postRequire("post","/area/findAreaByCorpCode","",param,function(data){
-
         if(data.code=="0"){
-            //$(".table tbody").empty();
-            //var message=JSON.parse(data.message);
-            //console.log(message);
-            //var list=message.all_vip_list;
-            //cout=message.pages;
-            ////var list=list.list;
-            //superaddition(list,a);
-            //jumpBianse();
-            //setPage($("#foot-num")[0],cout,a,b,funcCode);
-            console.log(data);//获取返回数据
             var message=JSON.parse(data.message);//获取messagejson对象的DOM对象
-            console.log(message);
-            console.log(message.list);
             var output=JSON.parse(message.list);
-            console.log(output);
-            console.log(output.list);//对象
             var ul='';
             var output_list=output.list;
+            output_list.length>7? $('#select_analyze s').attr('style','display:block'): $('#select_analyze s').attr('style','display:none');
             for(var i= 0;i<output_list.length;i++){
                 ul+="<li>"+output_list[i].area_name+"</li>";
             }
-            $('#select_analyze>ul').append(ul);
+            $('#select_analyze ul').append(ul);
         }else if(data.code=="-1"){
             alert(data.message);
         }
     });
 }
 GetArea();
-//显示selcet
-function show_select(){
+//绑定li的单击事件
+function areaClick(e){
+    var e= e.target;
+    $('#side_analyze ul li:nth-child(2) s').html($(e).html());
     $('#select_analyze').toggle();
+//    
 }
+//显示selcet
+function show_select(e){
+    $('#select_analyze').toggle();
+    //根据是否有area控制弹出框位置
+    $(e.target).attr('class').indexOf('area')==-1? $('#select_analyze').css('top','100px'): $('#select_analyze').css('top','69px');
+}
+$().ready(function(){
+    $('#side_analyze span').click(show_select);
+    $('#select_analyze ul').on('click','li',areaClick);
+});
