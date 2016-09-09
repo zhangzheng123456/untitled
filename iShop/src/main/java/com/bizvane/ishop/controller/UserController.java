6292c9cfc0a56d5410f32224c82114531b48a795
liuying8133 <liuying8133@163.com>
2016-09-09 16:33:22 +0800

    user

--- iShop/src/main/java/com/bizvane/ishop/controller/UserController.java
+++ iShop/src/main/java/com/bizvane/ishop/controller/UserController.java
@@ -477,7 +477,8 @@ public class UserController {
                     int b = 5 / 0;
                     break;
                 }
-
+            }
+            for(int i=3;i<column8.length;i++) {
                 String role = groupService.selRoleByGroupCode(column3[i].getContents().toString().trim(), column7[i].getContents().toString().trim());
                 if (role.equals(Common.ROLE_AM) || role.equals(Common.ROLE_SM) || role.equals(Common.ROLE_STAFF)) {
                     String areas = column8[i].getContents().toString().trim();
@@ -497,6 +498,9 @@ public class UserController {
                         }
                     }
                 }
+            }
+            for(int i=0;i<column9.length;i++){
+                String role = groupService.selRoleByGroupCode(column3[i].getContents().toString().trim(), column7[i].getContents().toString().trim());
                 if (role.equals(Common.ROLE_SM) || role.equals(Common.ROLE_STAFF)) {
                     String stores = column9[i].getContents().toString().trim();
                     String[] splitAreas = stores.split(",");
