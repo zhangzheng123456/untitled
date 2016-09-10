f1b5db0fec60caac102a5194937d5a536d2925c8
jiangyan <1042034123@qq.com>
2016-09-10 18:32:08 +0800

    消息快捷回复接口

---
+++ iShop/src/main/java/com/bizvane/ishop/service/MessageQuickReplyService.java
@@ -0,0 +1,32 @@
+package com.bizvane.ishop.service;
+
+import com.bizvane.ishop.entity.MessageQuickReply;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by nanji on 2016/9/10.
+ */
+public interface MessageQuickReplyService {
+
+    MessageQuickReply getQuickReplyById(int id) throws Exception;
+
+
+    PageInfo<MessageQuickReply> getAllQuickReplyByPage(int page_number, int page_size, String corp_code, String search_value) throws Exception;
+
+    List<MessageQuickReply> getAllQuickReply(String corp_code) throws Exception;
+
+    String insert(String message, String user_id) throws Exception;
+
+    String update(String message, String user_id) throws Exception;
+
+    int delete(int id) throws Exception;
+
+
+    MessageQuickReply getQuickReplyByCode(String corp_code, String code, String isactive) throws Exception;
+
+    PageInfo<MessageQuickReply> getAllQuickReplyScreen(int page_number, int page_size, String corp_code, Map<String, String> map) throws Exception;
+
+}
