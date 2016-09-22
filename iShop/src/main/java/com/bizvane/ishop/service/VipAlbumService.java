d214047dde92aa077164164644c16469017033fe
zhou <781174667@qq.com>
2016-09-22 09:36:06 +0800

    会员相册

--- iShop/src/main/java/com/bizvane/ishop/service/VipAlbumService.java
+++ iShop/src/main/java/com/bizvane/ishop/service/VipAlbumService.java
@@ -1,5 +1,6 @@
 package com.bizvane.ishop.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.bizvane.ishop.entity.VipAlbum;
 import com.github.pagehelper.PageInfo;
 
@@ -13,11 +14,14 @@ public interface VipAlbumService {
 
     PageInfo<VipAlbum> getAllVipAlbum(int page_num, int page_size, String corp_code, String search_value) throws Exception;
 
-    int insertVipAlbum(String message,String user_code) throws Exception;
+    int insertVipAlbum(JSONObject obj, String user_code) throws Exception;
 
     int deleteVipAlbum(int id) throws Exception;
 
     PageInfo<VipAlbum> getAllVipAlbumScreen(int page_number, int page_size, String corp_code, Map<String, String> map) throws Exception;
 
     List<VipAlbum> selectAlbumByVip(String corp_code, String vip_code) throws Exception;
+
+    VipAlbum selectAlbumByUrl(String image_url) throws Exception;
+
 }
