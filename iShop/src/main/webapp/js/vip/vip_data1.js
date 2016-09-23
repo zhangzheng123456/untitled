c4a8d5ccd8cd8a255377faeea031585a763b2d80
zhaowei <1174952035@qq.com>
2016-09-23 14:25:37 +0800

    会员信息

--- iShop/src/main/webapp/js/vip/vip_data1.js
+++ iShop/src/main/webapp/js/vip/vip_data1.js
@@ -10,7 +10,6 @@ function getVipInfo(){
         var vipDataList=vipData.list;
         var extend=vipData.extend;
         var extend_info=vipData.extend_info;
-        var sex=vipDataList.sex;
         var extendhtml="";
         function getvalue(){
             var VALUE="";
@@ -22,7 +21,6 @@ function getVipInfo(){
             return VALUE;
         }
         for(var i=0;i<extend.length;i++){
-            console.log(extend[i].name+extend[i].key+"文本类型"+extend[i].type);
             if(extend[i].type=="text"){
                 var value=getvalue();
                 extendhtml+="<li>"
@@ -69,32 +67,94 @@ function getVipInfo(){
             }
         }
         $("#extend ul").html(extendhtml);
-        if(sex=="male"){
-            $("#vip_name").next().addClass("icon-ishop_9-03");
-            $("#USER_SEX").val("男");
-        }else{
-            $("#vip_name").next().addClass("icon-ishop_9-02");
-            $("#USER_SEX").val("女");
-        }
-        $("#vip_name").html(vipDataList.vip_name);
-        $("#vip_name_edit").val(vipDataList.vip_name);
-        $("#vip_card_no").html(vipDataList.vip_card_no);
-        $("#vip_card_no_edit").val(vipDataList.vip_card_no);
-        $("#vip_card_type").html(vipDataList.vip_card_type);
-        $("#vip_card_type_edit").val(vipDataList.vip_card_type);
-        $("#vip_phone").html(vipDataList.vip_phone);
-        $("#vip_phone_edit").val(vipDataList.vip_phone);
-        $("#user_name").html(vipDataList.user_name);
-        $("#user_name_edit").val(vipDataList.user_name);
-        $("#vip_total_amount").html(vipDataList.total_amount);
-        $("#join_date").html(vipDataList.join_date);
-        $("#vip_dormant_time").html(vipDataList.dormant_time);
-        $("#vip_birthday").html(vipDataList.vip_birthday);
-        $("#vip_birthday_edit").val(vipDataList.vip_birthday);
+        fuzhi(vipDataList);
+        getVipPoints(vipDataList);
         showOption();
         getoselectvalue();
     })
 }
+
+function getVipPoints(data){
+    var parmam_jifen={};
+    parmam_jifen['vip_id']=sessionStorage.getItem("id");
+    parmam_jifen['corp_code']=data.corp_code;
+    parmam_jifen['type']=null;
+    oc.postRequire("post","/vip/vipPoints","",parmam_jifen,function(data){
+        var Data=JSON.parse(data.message);
+        var pointsData=Data.result_points;//积分
+        var consumnData=Data.result_consumn;//消费
+        var consumnlistData=JSON.parse(consumnData.list);//消费
+        var listData=JSON.parse(Data.result_points.list);//积分list
+        jifenContent(listData,pointsData);
+        var consumnHtml="";
+        $("#consume_total").html(consumnData.amount);
+        $("#amount_total").html(consumnData.times);
+        $("#vip_card_1").html(consumnData.vip_card_type);
+        $("#vip_card_num_1").html(consumnData.cardno);
+        $("#vip_name_1").html(consumnData.vip_name);
+        for(var i=0;i<consumnlistData.length;i++){
+            consumnHtml+='<tr>'
+            +'<td >'+consumnlistData[i].buy_time+'</td>'
+            +'<td>'+consumnlistData[i].order_count+'</td>'
+            +'<td>'+consumnlistData[i].order_discount+'</td>'
+            +'<td>'+consumnlistData[i].order_total+'<i class="icon-ishop_8-03 style"></i></td>'
+            +'</tr>'
+        }
+        $("#consum tbody").html(consumnHtml)
+    })
+}
+function jifenContent(listData,pointsData){
+    $("#points_total").html(pointsData.points);
+    $("#vip_card").html(pointsData.vip_card_type);
+    $("#vip_card_num").html(pointsData.cardno);
+    $("#vip_name1").html(pointsData.vip_name);
+    var listHtml="";
+    var listHtmlAll="";
+    for(var i=0;i<listData.length;i++){
+        var a=i+1;
+        if(i<10){
+            listHtml+='<tr>'
+                +'<td>'+listData[i].points+'</td>'
+                +'<td>'+listData[i].date+'<i class="icon-ishop_8-03 style"></i></td>'
+                +'</tr>'
+        }
+        listHtmlAll+='<tr>'
+            +'<td>'
+            +a+'</td>'
+            +'<td>'+listData[i].points+'</td>'
+            +'<td>'+listData[i].date+'</td>'
+            +'</tr>'
+    }
+    $("#points tbody").html(listHtml);
+    $("#points_all tbody").html(listHtmlAll)
+}
+function fuzhi(data){
+    var sex=data.sex;
+    if(sex=="male"){
+        $("#vip_name").next().addClass("icon-ishop_9-03");
+        $("#USER_SEX").val("男");
+    }else{
+        $("#vip_name").next().addClass("icon-ishop_9-02");
+        $("#USER_SEX").val("女");
+    }
+    $("#vip_name").html(data.vip_name);
+    $("#vip_name_edit").val(data.vip_name);
+    $("#vip_card_no").html(data.cardno);
+    $("#vip_card_no_edit").val(data.cardno);
+    $("#vip_card_type").html(data.vip_card_type);
+    $("#vip_card_type_edit").val(data.vip_card_type);
+    $("#vip_phone").html(data.vip_phone);
+    $("#vip_phone_edit").val(data.vip_phone);
+    $("#user_name").html(data.user_name);
+    $("#user_name_edit").val(data.user_name);
+    $("#vip_total_amount").html(data.total_amount);
+    $("#join_date").html(data.join_date);
+    $("#vip_dormant_time").html(data.dormant_time);
+    $("#vip_birthday").html(data.vip_birthday);
+    $("#vip_birthday_edit").val(data.vip_birthday);
+}
+
+
 $("#more_message").click(function(){
     gotovipallmessage();
 });
