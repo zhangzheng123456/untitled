480a6c6b47c850dd846e7397af8cd3258615cb61
yin <liuying8133@163.com>
2016-09-28 16:15:01 +0800

    导入控制列

--- iShop/src/main/java/com/bizvane/ishop/controller/UserAchvGoalController.java
+++ iShop/src/main/java/com/bizvane/ishop/controller/UserAchvGoalController.java
@@ -438,7 +438,7 @@ public class UserAchvGoalController {
         try {
             rwb = Workbook.getWorkbook(targetFile);
             Sheet rs = rwb.getSheet(0);//或者rwb.getSheet(0)
-            int clos = rs.getColumns();//得到所有的列
+            int clos = 7;//得到所有的列
             int rows = rs.getRows();//得到所有的行
 //            int actualRows = LuploadHelper.getRightRows(rs);
 //            if(actualRows != rows){
