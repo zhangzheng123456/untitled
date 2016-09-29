3eb1101e9f972f00f583ccd721e83d45aa0b29ec
zhou <781174667@qq.com>
2016-09-29 19:49:03 +0800

    no message

--- iShop/src/main/java/com/bizvane/ishop/controller/VipAnalysisController.java
+++ iShop/src/main/java/com/bizvane/ishop/controller/VipAnalysisController.java
@@ -237,7 +237,57 @@ public class VipAnalysisController {
             String query_type = jsonObject.get("type").toString();
             String time = jsonObject.get("time").toString();
 
-            Map datalist = iceInterfaceService.vipAnalysisBasicMethod(jsonObject,request);
+            String user_code = request.getSession().getAttribute("user_code").toString();
+            String corp_code = request.getSession().getAttribute("corp_code").toString();
+            String role_code = request.getSession().getAttribute("role_code").toString();
+            String user_id = "";
+            String area_code = "";
+            String store_id = "";
+            if (role_code.equals(Common.ROLE_SYS)) {
+                corp_code = jsonObject.get("corp_code").toString();
+            } else if (role_code.equals(Common.ROLE_GM)){
+                if (jsonObject.containsKey("area_code") && !jsonObject.get("area_code").toString().trim().equals("")){
+                    area_code = jsonObject.get("area_code").toString();
+                }
+            } else if (role_code.equals(Common.ROLE_AM) ){
+                if (jsonObject.containsKey("area_code") && !jsonObject.get("area_code").toString().trim().equals("")){
+                    area_code = jsonObject.get("area_code").toString();
+                }else {
+                    area_code = request.getSession().getAttribute("area_code").toString().replace(Common.SPECIAL_HEAD,"");
+                    String[] area_codes = area_code.split(",");
+                    area_code = area_codes[0];
+                }
+                if (jsonObject.containsKey("store_code") && !jsonObject.get("store_code").toString().trim().equals("")){
+                    store_id = jsonObject.get("store_code").toString();
+                }
+            } else if (role_code.equals(Common.ROLE_SM)){
+                if (jsonObject.containsKey("store_code") && !jsonObject.get("store_code").toString().trim().equals("")){
+                    store_id = jsonObject.get("store_code").toString();
+                }else {
+                    String store_code = request.getSession().getAttribute("store_code").toString().replace(Common.SPECIAL_HEAD, "");
+                    String[] store_codes = store_code.split(",");
+                    store_id = store_codes[0];
+                }
+            } else if (role_code.equals(Common.ROLE_STAFF)){
+                user_id = user_code;
+                if (jsonObject.containsKey("store_code") && !jsonObject.get("store_code").toString().trim().equals("")){
+                    store_id = jsonObject.get("store_code").toString();
+                }
+            }
+
+            Data data_user_id = new Data("user_id", user_id, ValueType.PARAM);
+            Data data_corp_code = new Data("corp_code", corp_code, ValueType.PARAM);
+            Data data_role_code = new Data("role_code", role_code, ValueType.PARAM);
+            Data data_store_id = new Data("store_id", store_id, ValueType.PARAM);
+            Data data_area_code = new Data("area_code", area_code, ValueType.PARAM);
+
+            Map datalist = new HashMap<String, Data>();
+            datalist.put(data_user_id.key, data_user_id);
+            datalist.put(data_corp_code.key, data_corp_code);
+            datalist.put(data_store_id.key, data_store_id);
+            datalist.put(data_area_code.key, data_area_code);
+            datalist.put(data_role_code.key, data_role_code);
+
 
             JSONObject all = new JSONObject();
             all.put("count","15675");
