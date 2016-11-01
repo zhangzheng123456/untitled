041f48995739d93e3c33d8fe283ec596fdb695bd
ghy <312169513@qq.com>
2016-11-01 09:13:46 +0800

    bug

--- iShop/src/main/webapp/js/goods/fab.js
+++ iShop/src/main/webapp/js/goods/fab.js
@@ -317,8 +317,9 @@ function GET(a, b) {
             var message = JSON.parse(data.message);
             var list = JSON.parse(message.list);
             cout = list.pages;
+            var pageNum = list.pageNum;
             var list = list.list;
-            superaddition(list, a);
+            superaddition(list, pageNum);
             jumpBianse();
             setPage($("#foot-num")[0], cout, a, b, funcCode);
         } else if (data.code == "-1") {
@@ -441,6 +442,7 @@ function POST(a, b) {
             var message = JSON.parse(data.message);
             var list = JSON.parse(message.list);
             cout = list.pages;
+            var pageNum = list.pageNum;
             var list = list.list;
             var actions = message.actions;
             $(".table tbody").empty();
@@ -450,7 +452,7 @@ function POST(a, b) {
                 whir.loading.remove();//移除加载框
             } else if (list.length > 0) {
                 $(".table p").remove();
-                superaddition(list, a);
+                superaddition(list, pageNum);
                 jumpBianse();
             }
             var input = $(".inputs input");
@@ -829,6 +831,7 @@ function filtrates(a, b) {
             var message = JSON.parse(data.message);
             var list = JSON.parse(message.list);
             cout = list.pages;
+            var pageNum = list.pageNum;
             var list = list.list;
             var actions = message.actions;
             $(".table tbody").empty();
@@ -838,7 +841,7 @@ function filtrates(a, b) {
                 whir.loading.remove();//移除加载框
             } else if (list.length > 0) {
                 $(".table p").remove();
-                superaddition(list, a);
+                superaddition(list, pageNum);
                 jumpBianse();
             }
             setPage($("#foot-num")[0], cout, a, b, funcCode);
