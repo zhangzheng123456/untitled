25f6dd99931d33cbae0843adbc6d288f533f4848
lishun <773538995@qq.com>
2016-11-01 15:41:36 +0800

    修改

--- iShop/src/main/webapp/js/message/message.js
+++ iShop/src/main/webapp/js/message/message.js
@@ -218,10 +218,8 @@ function superaddition(data, num) {//页面加载循环
             // + data[i].message_receiver
             // + "</td><td>"
             + data[i].message_type
-            + "</td><td  class='message_code' data-code='"+data[i].message_code+"'>"
-            + receiver_type
             + "</td><td >"
-            + data[i].receiver_type
+            + receiver_type
             + "</td><td  class='message_code' data-code='"+data[i].message_code+"'>"
             + data[i].message_title
             + "</td><td><span title='" + data[i].message_content + "'>"
