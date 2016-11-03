f4e51a60dc26009b2b2487bb80f792c0b802e492
lishun <773538995@qq.com>
2016-11-03 10:54:09 +0800

    左侧导航栏  禁止复制

--- iShop/src/main/webapp/goods/mobile/js/recommend.js
+++ iShop/src/main/webapp/goods/mobile/js/recommend.js
@@ -146,20 +146,33 @@ jQuery(function(){
 	}
 	getList(rowno);
 	//input输入框里面
-	$('#input').bind('input propertychange', function() {
-	    var thatFun=arguments.callee;
-        var that=this;
-        $(this).unbind("input propertychange",thatFun);
-		value=$('#input').val().replace(/\s+/g,"");
-		$(".screen_content .list li").attr("class","");
-		param["search_value"]=value;
-		param["brand_code"]="";
-		param["goods_quarter"]="";
-		param["goods_wave"]="";
-		jQuery('.allShops').empty();
-		getSearchList(rowno);
-		setTimeout(function(){$(that).bind("input propertychange",thatFun)},0);
-	});
+	// $('#input').bind('input propertychange', function() {
+	//     var thatFun=arguments.callee;
+ //        var that=this;
+ //        $(this).unbind("input propertychange",thatFun);
+	// 	value=$('#input').val().replace(/\s+/g,"");
+	// 	$(".screen_content .list li").attr("class","");
+	// 	param["search_value"]=value;
+	// 	param["brand_code"]="";
+	// 	param["goods_quarter"]="";
+	// 	param["goods_wave"]="";
+	// 	jQuery('.allShops').empty();
+	// 	getSearchList(rowno);
+	// 	setTimeout(function(){$(that).bind("input propertychange",thatFun)},0);
+	// });
+	$("#input").keydown(function() {
+	    var event=window.event||arguments[0];
+	    if(event.keyCode == 13){
+	        value=$('#input').val().trim();
+			$(".screen_content .list li").attr("class","");
+			param["search_value"]=value;
+			param["brand_code"]="";
+			param["goods_quarter"]="";
+			param["goods_wave"]="";
+			jQuery('.allShops').empty();
+			getSearchList(rowno);
+	    }
+    });
 	//搜索加载list
 	function getSearchList(a){
 		param["rowno"]=a;
