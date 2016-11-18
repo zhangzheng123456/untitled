8436c5f4bfc4b82a4f75216905d04d8c164cdeb5
huxue <huxue_pd@163.com>
2016-11-18 10:32:59 +0800

    仅显示已完成&仅显示未完成

--- iShop/src/main/webapp/js/activity/activity_details.js
+++ iShop/src/main/webapp/js/activity/activity_details.js
@@ -1,15 +1,50 @@
 /**
  * Created by Administrator on 2016/11/16.
  */
+//仅显示已完成
+$('#showDone').click(function(){
+    listShow();
+    $(".percent_percent").each(function(){
+        var nowVal=($(this).text().replace('%',''));
+        if(nowVal<100){
+            $(this).parents('.people_title ').hide();
+            console.log('隐藏了。。。！');
+        }
+    })
+});
+//显示未完成
+$('#showDoing').click(function(){
+    listShow();
+    $(".percent_percent").each(function(){
+        var nowVal=($(this).text().replace('%',''));
+        if(nowVal==100||nowVal==0){
+            $(this).parents('.people_title ').hide();
+            console.log('隐藏了。。。！');
+        }
+    })
+})
 //员工列表加载
 function listShow(){
     var name = '张某某';
-    var num = '10000';
+    var num = '100000';
     var area = '华东一区';
     var shop='上海敏行分店一店';
-    var percent = Math.floor(Math.random()*40+60);
     var tempHTML = '<li class="people_title"> <div class="people_title_order" style="text-align: center">${order}</div> <div class="people_title_name">${name}</div> <div class="people_title_num">${num}</div> <div class="people_title_area">${area}</div> <div class="people_title_shop">${shop}</div> <div class="people_title_plan"> <div class="undone"><div class="done"></div></div><span class="percent_percent">${percent}%</span></div> </li>';
     for(i=0;i<11;i++){
+        //随机进度
+        var percent = Math.floor(Math.random()*40+60);
+        var html = '';
+        var order = i+1;
+        var nowHTML1 = tempHTML;
+        nowHTML1 = nowHTML1.replace("${order}", order);
+        nowHTML1 = nowHTML1.replace("${name}", name);
+        nowHTML1 = nowHTML1.replace("${num}", num);
+        nowHTML1 = nowHTML1.replace("${area}", area);
+        nowHTML1 = nowHTML1.replace("${shop}", shop);
+        $('.done').css('width',percent+'%');
+        nowHTML1 = nowHTML1.replace("${percent}", percent);
+        html += nowHTML1;
+        $("#peopleContent").append(html);
         //判断进度颜色
         if(percent<100&&percent>0){
             $('.percent_percent').css('color','#42a0a8');
@@ -18,20 +53,9 @@ function listShow(){
         }else if(percent==0){
             $('.percent_percent').css('color','red');
         }
-        var html = '';
-        var order = i+1;
-        var nowHTML1 = tempHTML;
-        nowHTML1 = nowHTML1.replace("${order}", order);
-        nowHTML1 = nowHTML1.replace("${name}", order);
-        nowHTML1 = nowHTML1.replace("${num}", num);
-        nowHTML1 = nowHTML1.replace("${area}", area);
-        nowHTML1 = nowHTML1.replace("${shop}", shop);
-        $('.done').css('width',percent+'%');
-        nowHTML1 = nowHTML1.replace("${percent}", percent);
-        html += nowHTML1;
-        $("#peopleContent").append(html);
     }
 }
+//页面加载假数据
 window.onload = function(){
     var beiginTime = '2016-9-12';
     var endTime = '2016-12-12'
