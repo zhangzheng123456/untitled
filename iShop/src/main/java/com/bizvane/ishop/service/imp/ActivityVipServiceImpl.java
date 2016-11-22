5370735af541af5f16b9a75cdba1b21e67d6cc8d
zhou <781174667@qq.com>
2016-11-22 10:33:01 +0800

    会员活动

--- iShop/src/main/java/com/bizvane/ishop/service/imp/ActivityVipServiceImpl.java
+++ iShop/src/main/java/com/bizvane/ishop/service/imp/ActivityVipServiceImpl.java
@@ -68,25 +68,27 @@ public class ActivityVipServiceImpl implements ActivityVipService {
         String result = null;
         org.json.JSONObject jsonObject = new org.json.JSONObject(message);
         String corp_code = jsonObject.get("corp_code").toString().trim();
-        String activity_theme = jsonObject.get("activity_theme").toString().trim();
-        String run_mode = jsonObject.get("run_mode").toString().trim();
-        String start_time = jsonObject.get("start_time").toString().trim();
-        String end_time = jsonObject.get("end_time").toString().trim();
-        String target_vips = jsonObject.get("target_vips").toString().trim();
-        String operators = jsonObject.get("operators").toString().trim();
-        //任务类型
-        String task_title = jsonObject.get("task_title").toString().trim();
-        String task_desc = jsonObject.get("task_desc").toString().trim();
-        //公众号消息推送
-        String wechat_title = jsonObject.get("wechat_title").toString().trim();
-        String wechat_desc = jsonObject.get("wechat_desc").toString().trim();
-        //短信
-        String msg_info = jsonObject.get("msg_info").toString().trim();
+        String activity_code = jsonObject.get("activity_code").toString().trim();
 
-        //活动内容
-        String activity_url = jsonObject.get("activity_url").toString().trim();
-       // String activity_content = jsonObject.get("activity_content").toString().trim();
-        String content_url = jsonObject.get("content_url").toString().trim();
+//        String activity_theme = jsonObject.get("activity_theme").toString().trim();
+//        String run_mode = jsonObject.get("run_mode").toString().trim();
+//        String start_time = jsonObject.get("start_time").toString().trim();
+//        String end_time = jsonObject.get("end_time").toString().trim();
+//        String target_vips = jsonObject.get("target_vips").toString().trim();
+//        String operators = jsonObject.get("operators").toString().trim();
+//        //任务类型
+//        String task_title = jsonObject.get("task_title").toString().trim();
+//        String task_desc = jsonObject.get("task_desc").toString().trim();
+//        //公众号消息推送
+//        String wechat_title = jsonObject.get("wechat_title").toString().trim();
+//        String wechat_desc = jsonObject.get("wechat_desc").toString().trim();
+//        //短信
+//        String msg_info = jsonObject.get("msg_info").toString().trim();
+//
+//        //活动内容
+//        String activity_url = jsonObject.get("activity_url").toString().trim();
+//       // String activity_content = jsonObject.get("activity_content").toString().trim();
+//        String content_url = jsonObject.get("content_url").toString().trim();
         String activity_state = "未执行";
 
         ActivityVip activityVip = WebUtils.JSON2Bean(jsonObject, ActivityVip.class);
@@ -107,36 +109,35 @@ public class ActivityVipServiceImpl implements ActivityVipService {
             LuploadHelper.deleteFile(path+"/"+htmlImageSrcList.get(k));
         }
         Date now = new Date();
-        activityVip.setCorp_code(corp_code);
-        activityVip.setActivity_theme(activity_theme);
-        activityVip.setRun_mode(run_mode);
-        activityVip.setStart_time(start_time);
-        activityVip.setEnd_time(end_time);
-        activityVip.setTarget_vips(target_vips);
-        activityVip.setOperators(operators);
-
-        activityVip.setTask_title(task_title);
-        activityVip.setTask_desc(task_desc);
-
-        activityVip.setWechat_title(wechat_title);
-        activityVip.setWechat_desc(wechat_desc);
-
-        activityVip.setMsg_info(msg_info);
-
-        activityVip.setActivity_url(activity_url);
+//        activityVip.setCorp_code(corp_code);
+//        activityVip.setActivity_theme(activity_theme);
+//        activityVip.setRun_mode(run_mode);
+//        activityVip.setStart_time(start_time);
+//        activityVip.setEnd_time(end_time);
+//        activityVip.setTarget_vips(target_vips);
+//        activityVip.setOperators(operators);
+//
+//        activityVip.setTask_title(task_title);
+//        activityVip.setTask_desc(task_desc);
+//
+//        activityVip.setWechat_title(wechat_title);
+//        activityVip.setWechat_desc(wechat_desc);
+//
+//        activityVip.setMsg_info(msg_info);
+//
+//        activityVip.setActivity_url(activity_url);
         activityVip.setActivity_content(activity_content);
-        activityVip.setContent_url(content_url);
+//        activityVip.setContent_url(content_url);
         activityVip.setModifier(user_id);
         activityVip.setModified_date(Common.DATETIME_FORMAT.format(now));
         activityVip.setCreater(user_id);
         activityVip.setCreated_date(Common.DATETIME_FORMAT.format(now));
-        activityVip.setIsactive(jsonObject.get("isactive").toString().trim());
+//        activityVip.setIsactive(jsonObject.get("isactive").toString().trim());
         activityVip.setActivity_state(activity_state);
         activityVip.setTask_code("");
         int info=0;
          info= activityVipMapper.insertActivity(activityVip);
-        ActivityVip activityVip1 = this.getActivityForId(corp_code, activityVip.getActivity_theme(), activityVip.getRun_mode(), activityVip.getCreated_date());
-
+        ActivityVip activityVip1 = selActivityByCode(corp_code, activity_code);
         if (info>0) {
             //result=String.valueOf(activityVip1.getId());
             return String.valueOf(activityVip1.getId());
@@ -153,21 +154,21 @@ public class ActivityVipServiceImpl implements ActivityVipService {
         int activity_id = Integer.parseInt(jsonObject.get("id").toString().trim());
 
         String corp_code = jsonObject.get("corp_code").toString().trim();
-        String activity_theme = jsonObject.get("activity_theme").toString().trim();
-        String run_mode = jsonObject.get("run_mode").toString().trim();
-        String start_time = jsonObject.get("start_time").toString().trim();
-        String end_time = jsonObject.get("end_time").toString().trim();
-       String target_vips = jsonObject.get("target_vips").toString().trim();
-        String operators = jsonObject.get("operators").toString().trim();
-        String task_title = jsonObject.get("task_title").toString().trim();
-        String task_desc = jsonObject.get("task_desc").toString().trim();
-        String wechat_title = jsonObject.get("wechat_title").toString().trim();
-        String wechat_desc = jsonObject.get("wechat_desc").toString().trim();
-        String activity_url = jsonObject.get("activity_url").toString().trim();
+//        String activity_theme = jsonObject.get("activity_theme").toString().trim();
+//        String run_mode = jsonObject.get("run_mode").toString().trim();
+//        String start_time = jsonObject.get("start_time").toString().trim();
+//        String end_time = jsonObject.get("end_time").toString().trim();
+//       String target_vips = jsonObject.get("target_vips").toString().trim();
+//        String operators = jsonObject.get("operators").toString().trim();
+//        String task_title = jsonObject.get("task_title").toString().trim();
+//        String task_desc = jsonObject.get("task_desc").toString().trim();
+//        String wechat_title = jsonObject.get("wechat_title").toString().trim();
+//        String wechat_desc = jsonObject.get("wechat_desc").toString().trim();
+//        String activity_url = jsonObject.get("activity_url").toString().trim();
      // String activity_content = jsonObject.get("activity_content").toString().trim();
-        String content_url = jsonObject.get("content_url").toString().trim();
+//        String content_url = jsonObject.get("content_url").toString().trim();
 
-        String task_code = jsonObject.get("task_code").toString().trim();
+//        String task_code = jsonObject.get("task_code").toString().trim();
         String path="";
         ActivityVip activityVip = WebUtils.JSON2Bean(jsonObject, ActivityVip.class);
         String activity_content = activityVip.getActivity_content();
@@ -187,30 +188,28 @@ public class ActivityVipServiceImpl implements ActivityVipService {
         }
         Date now = new Date();
         activityVip.setId(activity_id);
-        activityVip.setCorp_code(corp_code);
-        activityVip.setActivity_theme(activity_theme);
-        activityVip.setRun_mode(run_mode);
-        activityVip.setStart_time(start_time);
-        activityVip.setEnd_time(end_time);
-        activityVip.setTarget_vips(target_vips);
-        activityVip.setOperators(operators);
-        activityVip.setTask_title(task_title);
-        activityVip.setTask_desc(task_desc);
-        activityVip.setWechat_title(wechat_title);
-        activityVip.setWechat_desc(wechat_desc);
-        activityVip.setActivity_url(activity_url);
+//        activityVip.setCorp_code(corp_code);
+//        activityVip.setActivity_theme(activity_theme);
+//        activityVip.setRun_mode(run_mode);
+//        activityVip.setStart_time(start_time);
+//        activityVip.setEnd_time(end_time);
+//        activityVip.setTarget_vips(target_vips);
+//        activityVip.setOperators(operators);
+//        activityVip.setTask_title(task_title);
+//        activityVip.setTask_desc(task_desc);
+//        activityVip.setWechat_title(wechat_title);
+//        activityVip.setWechat_desc(wechat_desc);
+//        activityVip.setActivity_url(activity_url);
         activityVip.setActivity_content(activity_content);
-        activityVip.setContent_url(content_url);
+//        activityVip.setContent_url(content_url);
         activityVip.setModifier(user_id);
         activityVip.setModified_date(Common.DATETIME_FORMAT.format(now));
-        activityVip.setCreater(user_id);
-        activityVip.setCreated_date(Common.DATETIME_FORMAT.format(now));
-        activityVip.setIsactive(jsonObject.get("isactive").toString().trim());
-        activityVip.setTask_code(task_code);
+//        activityVip.setCreater(user_id);
+//        activityVip.setCreated_date(Common.DATETIME_FORMAT.format(now));
+//        activityVip.setIsactive(jsonObject.get("isactive").toString().trim());
+//        activityVip.setTask_code(task_code);
         int info=0;
         info= activityVipMapper.updateActivity(activityVip);
-        ActivityVip activityVip1 = this.getActivityForId(activityVip.getCorp_code(), activityVip.getActivity_theme(), activityVip.getRun_mode(), activityVip.getCreated_date());
-
         if (info>0) {
             result=Common.DATABEAN_CODE_SUCCESS;
             return result;
@@ -231,13 +230,8 @@ public class ActivityVipServiceImpl implements ActivityVipService {
     }
 
     @Override
-    public ActivityVip getActivityForId(String corp_code, String activity_theme, String run_mode, String created_date) throws Exception {
-        return activityVipMapper.getActivityForID( corp_code,  activity_theme,  run_mode, created_date);
-    }
-
-    @Override
-    public ActivityVip selActivityByCode(String corp_code, String activity_code) throws Exception {
-        return activityVipMapper.selActivityByCode( corp_code, activity_code);
+    public ActivityVip selActivityByCode(String corp_code, String activity_vip_code) throws Exception {
+        return activityVipMapper.selActivityByCode( corp_code, activity_vip_code);
     }
 
 }
