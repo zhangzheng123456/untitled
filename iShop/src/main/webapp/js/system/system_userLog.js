f2c05cc678fffee5c8f34fbcdfa441e6c98dd135
huxue <huxue_pd@163.com>
2016-11-23 10:14:53 +0800

    用户日志-删除功能

--- iShop/src/main/webapp/js/system/system_userLog.js
+++ iShop/src/main/webapp/js/system/system_userLog.js
@@ -235,7 +235,7 @@ function superaddition(data,num){//页面加载循环
         }else{
             var a=i+1;
         }
-        $(".table tbody").append("<tr id='"+data[i]._id+"''><td width='50px;' style='text-align: left;'><div class='checkbox'><input  type='checkbox' value='' name='test' title='全选/取消' class='check'  id='checkboxTwoInput"
+        $(".table tbody").append("<tr id='"+data[i].id+"''><td width='50px;' style='text-align: left;'><div class='checkbox'><input  type='checkbox' value='' name='test' title='全选/取消' class='check'  id='checkboxTwoInput"
             + i
             + 1
             + "'/><label for='checkboxTwoInput"
@@ -295,19 +295,19 @@ function GET(a,b){
         if(data.code=="0"){
             $(".table tbody").empty();
             var message=JSON.parse(data.message);
-            //console.log('message是:'+JSON.stringify(message));
+            console.log('message是:'+JSON.stringify(message));
             var list=message.list;
-            //console.log('message是:'+JSON.stringify(list));
+            console.log('message是:'+JSON.stringify(list));
             var page_number = message.page_number;
-            console.log('页面是：'+page_number+  '  '   +"list是："+list);
+            console.log('页面是：'+page_number+  '  '   +"list是："+JSON.stringify(list));
             superaddition(list,page_number);
             jumpBianse();
             for(i=0;i<list.length;i++) {
                 var funcCode = list[i].code;
-                var _id = list[i]._id;
+                var id = list[i].id;
                 var cout = list.length;
-                //console.log(funcCode+''+action+''+JSON.stringify(_id)+'________'+cout);
                 setPage($("#foot-num")[0], cout, page_number, b, funcCode);
+                console.log(funcCode+''+'___________'+id+'________'+cout);
             }
 
         }else if(data.code=="-1"){
@@ -601,6 +601,7 @@ $('#file_close').click(function(){
 })
 //筛选按钮
 oc.postRequire("get","/userOperation/screen?funcCode="+funcCode+"","0","",function(data){
+    console.log(funcCode);
     if(data.code=="0"){
         var message=JSON.parse(data.message);
         console.log(JSON.stringify(message));
