a2f743a9ef4c97c69423e3f83dde4fda5c9a420f
ghy <312169513@qq.com>
2016-12-02 15:09:24 +0800

    bug

--- iShop/src/main/webapp/js/goods/fab_match.js
+++ iShop/src/main/webapp/js/goods/fab_match.js
@@ -271,7 +271,7 @@ $("#d_search").click(function () {
 
 //搜索的请求函数
 function POST(a, b) {
-    whir.loading.add("", 1);//加载等待框
+    whir.loading.add("", 0.5);//加载等待框
     oc.postRequire("post", "/defmatch/search", "0", param, function (data) {
         if (data.code == "0") {
             var message = JSON.parse(data.message);
