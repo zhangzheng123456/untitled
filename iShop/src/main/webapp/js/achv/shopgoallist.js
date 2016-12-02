97a302e1ff3afc7a93c6dbeea9fd4bdda55172e3
ghy <312169513@qq.com>
2016-12-02 17:29:20 +0800

    bug

--- iShop/src/main/webapp/js/achv/shopgoallist.js
+++ iShop/src/main/webapp/js/achv/shopgoallist.js
@@ -714,7 +714,7 @@ oc.postRequire("get","/list/filter_column?funcCode="+funcCode+"","0","",function
         var li="";
         for(var i=0;i<filter.length;i++){
             if(filter[i].type=="text"){
-                if(filter[i].show_name=="时间段"){
+                if(filter[i].show_name=="目标日期"){
                     li += "<li><label>" + filter[i].show_name + "</label><input type='text' id='" + filter[i].col_name + "' class='laydate-icon' onclick='laydate({istime: true, format: \"YYYY-MM-DD\"})'></li>";
                 }else {
                     li += "<li><label>" + filter[i].show_name + "</label><input type='text' id='" + filter[i].col_name + "'></li>";
