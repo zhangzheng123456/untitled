f1b8d00c8942ab3d124467017be40ea75d735fed
lishun <773538995@qq.com>
2016-12-08 14:47:58 +0800

    权限修改

--- iShop/src/main/webapp/js/user/groupcheck_power1.js
+++ iShop/src/main/webapp/js/user/groupcheck_power1.js
@@ -10,6 +10,45 @@ var groupPower={
 	},
     pageRendering:function(list){//绘制页面
     	console.log(list);
+    	var tr="";
+    	for(var i=0;i<list.length;i++){
+    		var num=i+1;
+    		tr+="<tr data-function='"+list[i].function_code+"'><td style='text-align: left;padding-left:22px;width:7.21%;'>"
+    		+num+"</td><td style='width:12.78%;'>"+list[i].module_name+"</td><td style='width: 12.78%;'>"+list[i].function_name+
+    		"</td><td style='width: 22.69%;'><div class='action_name'><ul>"
+    		for(var j=0;j<list[i].actions.length;j++){
+                var color="";
+                if(list[i].actions[j].is_die=="Y"){
+                    color="die";
+                }
+                if(list[i].actions[j].is_die=="N"&&list[i].actions[j].is_live=="N"){
+                    color="";
+                }
+                if(list[i].actions[j].is_die=="N"&&list[i].actions[j].is_live=="Y"){
+                    color="active";
+                }
+    			tr+="<li class='"+color+"' data-actionCode='"+list[i].actions[j].action_code+"' data-actionName='"+list[i].actions[j].action_name+
+    			"' data-actionId='"+list[i].actions[j].action_id+"'>"+
+    			list[i].actions[j].show_name+"</li>"
+    		}
+    		tr+="</ul></div></td><td style='width: 44.54%;'><div class='modify_options'><ul>";
+    		for(var k=0;k<list[i].columns.length;k++){
+                var color="";
+                if(list[i].columns[k].is_die=="Y"){
+                    color="die";
+                }
+                if(list[i].columns[k].is_die=="N"&&list[i].columns[k].is_live=="N"){
+                    color="";
+                }
+                if(list[i].columns[k].is_die=="N"&&list[i].columns[k].is_live=="Y"){
+                    color="active";
+                }
+    			tr+="<li class='"+color+"' data-columnId='"+list[i].columns[k].column_id+"' data-columnName='"+list[i].columns[k].column_name+"'>"+
+    			list[i].columns[k].show_name+"</li>"
+    		}
+    		tr+="</ul></div></td></tr>";
+    	}
+    	$("#table tbody").html(tr);
     },
 	getPowerlist:function(){
 		var self=this;
@@ -24,9 +63,18 @@ var groupPower={
         });
 	},
 	close:function(){
+        var self=this;
 		$("#turnoff").bind("click",function(){
 			$(window.parent.document).find('#iframepage').attr("src","/user/group_edit.html");
 		});
+        $(".power_table").on("click","ul li",function(){
+            var class_name=$(this).attr("class");
+            if(class_name=="die"){
+                return;
+            }else{
+                $(this).toggleClass("active");
+            }
+        })
 	}
 };
 $(function(){
