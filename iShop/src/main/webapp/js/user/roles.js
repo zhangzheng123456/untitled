ad297bbbbcdb93616f016fa171cd730c88487206
zhaowei <1174952035@qq.com>
2016-12-20 09:32:50 +0800

    权限管理——删除列表成功时的弹框和加载框冲突的问题

--- iShop/src/main/webapp/js/user/roles.js
+++ iShop/src/main/webapp/js/user/roles.js
@@ -476,13 +476,15 @@ $("#delete").click(function(){
     oc.postRequire("post","/user/role/delete","0",param,function(data){
         if(data.code=="0"){
             if(value==""){
-               frame();
+               frame().then(function(){
+                   GET();
+               });
                $('.frame').html('删除成功');
-               GET(); 
             }else if(value!==""){
-               frame();
+               frame().then(function(){
+                   POST();
+               });
                $('.frame').html('删除成功');
-               POST();
             }
         var thinput=$("thead input")[0];
         thinput.checked =false;
@@ -494,12 +496,18 @@ $("#delete").click(function(){
 })
 //删除弹框
  function frame(){
+     var def= $.Deferred();
     var left=($(window).width()-$("#frame").width())/2;//弹框定位的left值
     var tp=($(window).height()-$("#frame").height())/2;//弹框定位的top值
     $('.frame').remove();
     $('.content').append('<div class="frame" style="left:'+left+'px;top:'+tp+'px;"></div>');
     $(".frame").animate({opacity:"1"},1000);
     $(".frame").animate({opacity:"0"},1000);
+     setTimeout(function(){
+         $(".frame").hide();
+         def.resolve();
+     },2000);
+     return def;
 } 
 //全选
 function checkAll(name){
