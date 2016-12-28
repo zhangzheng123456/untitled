c505c250497e8d048184623439da741992f68bac
huxue <huxue_pd@163.com>
2016-12-28 10:40:06 +0800

    no message

---
+++ iShop/src/main/webapp/goods/mobile/js/tie-inList.js
@@ -0,0 +1,96 @@
+/**
+ * Created by huxue on 2016/12/28.
+ */
+var oc = new ObjectControl();
+//    选项卡-推荐
+$('.title div').eq(0).click(function () {
+    $('.main').eq(0).css('display','block');
+    $('.main').eq(1).css('display','none');
+    $('.title div').eq(0).css('color','#6dc1c8');
+    $('.title div').eq(0).css('background-color','white');
+    $('.title div').eq(1).css('color','#888888');
+    $('.title div').eq(1).css('background-color','#ededed');
+});
+//    选项卡-我的
+$('.title div').eq(1).click(function () {
+    $('.main').eq(0).css('display','none');
+    $('.main').eq(1).css('display','block');
+    $('.title div').eq(1).css('color','#6dc1c8');
+    $('.title div').eq(1).css('background-color','white');
+    $('.title div').eq(0).css('color','#888888');
+    $('.title div').eq(0).css('background-color','#ededed');
+});
+
+//    页面加载获取数据
+function getVal(){
+    var num = '5';
+    pageVal(num);
+}
+function pageVal(num){
+    var tempHTML = '<ul class="goods_box"> <li class="the_img"><img src="${img}" alt=""/></li> <li class="the_list"> <img src="${img}" alt=""/> <img src="${img}" alt=""/> <img src="${img}" alt=""/> <span class="num">${num}</span> </li> <li class="add"> <div><img src="image/icon_点赞@2x.png" alt="点赞"/><span class="add_num">${num}</span></div> <div><img src="image/icon_评论@2x.png" alt="评论"/><span class="add_num">${num}</span></div> <div><img src="image/icon_收藏@2x.png" alt="收藏"/><span class="add_num">${num}</span></div> </li> </ul>';
+    var html = ''
+    for(i=0;i<10;i++){
+        var nowHTML = tempHTML;
+        nowHTML = nowHTML.replace('${img}','image/list_item_00.jpg');
+        nowHTML = nowHTML.replace('${img}','image/list_item_01.jpg');
+        nowHTML = nowHTML.replace('${img}','image/list_item_02.jpg');
+        nowHTML = nowHTML.replace('${img}','image/list_item_02.jpg');
+        nowHTML = nowHTML.replace('${num}',num);
+        nowHTML = nowHTML.replace('${num}',num);
+        nowHTML = nowHTML.replace('${num}',num);
+        nowHTML = nowHTML.replace('${num}',num);
+        html+= nowHTML;
+        $('.main').eq(0).html(html);
+        if(i<2){
+            $('.main').eq(1).html(html);
+        }
+    }
+}
+function setTime(){
+    var val =  $('.the_img').width();
+    $('.the_img').css('height',val);
+    $('.the_img img').css('height',val);
+    var listVal = $('.the_list img').width();
+    $('.the_list img').css('height',listVal);
+    $('.the_list').css('height',listVal);
+}
+
+//    页面加载
+window.onload = function () {
+    $('.title div').eq(0).click();
+    getVal();
+    setTime()
+    $('.add div img').click(function () {
+        var src = $(this).attr("src");
+        //    点赞
+        if(src =='image/icon_点赞@2x.png'){
+            $(this).attr('src','image/icon_点赞_已点赞@2x.png');
+            var val = $(this).next('.add_num').text();
+            $(this).next('.add_num').text(parseInt(val)+1);
+        }else if (src =='image/icon_点赞_已点赞@2x.png'){
+            $(this).attr('src','image/icon_点赞@2x.png');
+            var val = $(this).next('.add_num').text();
+            $(this).next('.add_num').text(parseInt(val)-1);
+        }
+        //    评论
+        if(src =='image/icon_评论@2x.png'){
+            alert('暂无该功能')
+        }
+        //    收藏
+        if(src =='image/icon_收藏@2x.png'){
+            $(this).attr('src','image/icon_收藏_已收藏@2x.png');
+            var val = $(this).next('.add_num').text();
+            $(this).next('.add_num').text(parseInt(val)+1);
+        }else if (src =='image/icon_收藏_已收藏@2x.png'){
+            $(this).attr('src','image/icon_收藏@2x.png');
+            var val = $(this).next('.add_num').text();
+            $(this).next('.add_num').text(parseInt(val)-1);
+        }
+    });
+    $('.the_img img').click(function () {
+        window.location = 'details.html';
+    })
+    $('.the_list img').click(function () {
+        window.location = 'details.html';
+    })
+}
