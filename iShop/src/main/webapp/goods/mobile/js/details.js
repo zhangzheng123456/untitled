46118cbefeae03e2b7b1166c834ccd298979d8ef
huxue <huxue_pd@163.com>
2016-12-28 17:07:28 +0800

    接口

--- iShop/src/main/webapp/goods/mobile/js/details.js
+++ iShop/src/main/webapp/goods/mobile/js/details.js
@@ -1,7 +1,8 @@
 /**
  * Created by huxue on 2016/12/28.
  */
-//    Ñ¡Ïî¿¨
+var oc = new ObjectControl();
+//    é€‰é¡¹å¡
 $('.main_select div').click(function () {
     $('.main_select div').css('background-color','#ededed');
     $(this).css('background-color','white');
@@ -14,34 +15,68 @@ $('.main_select div').eq(1).click(function () {
     $('.main_content').eq(1).css('display','block');
     $('.main_content').eq(0).css('display','none');
 });
-//    µ×²¿
+//    ç‚¹èµ-æ”¶è—-è¯„è®º
 $('.bottom div img').click(function () {
     var src = $(this).attr("src");
-    //    µãÔŞ
-    if(src =='image/icon_µãÔŞ@2x.png'){
-        $(this).attr('src','image/icon_µãÔŞ_ÒÑµãÔŞ@2x.png');
-        var val = $(this).next('.num').text();
-        console.log(val);
-        $(this).next('.num').text(parseInt(val)+1);
-    }else if (src =='image/icon_µãÔŞ_ÒÑµãÔŞ@2x.png'){
-        $(this).attr('src','image/icon_µãÔŞ@2x.png');
-        var val = $(this).next('.num').text();
-        $(this).next('.num').text(parseInt(val)-1);
-    }
-    //    ÆÀÂÛ
-    if(src =='image/icon_ÆÀÂÛ@2x.png'){
-        alert('ÔİÎŞ¸Ã¹¦ÄÜ')
-    }
-    //    ÊÕ²Ø
-    if(src =='image/icon_ÊÕ²Ø@2x.png'){
-        $(this).attr('src','image/icon_ÊÕ²Ø_ÒÑÊÕ²Ø@2x.png');
+    var corp_code = 'C10000';
+    var d_match_code = '10000';  //ç§€æ­ç¼–å·
+    var operate_userCode = '10000'; //æ“ä½œäººuser_code
+    var operate_type = '';//type
+    var comment_text = '';//è¯„è®ºå†…å®¹
+    var status = '';  //æ˜¯å¦ç‚¹èµoræ”¶è—
+    if(src =='image/icon_ç‚¹èµ@2x.png'){
+        $(this).attr('src','image/icon_ç‚¹èµ_å·²ç‚¹èµ@2x.png');
         var val = $(this).next('.num').text();
         $(this).next('.num').text(parseInt(val)+1);
-    }else if (src =='image/icon_ÊÕ²Ø_ÒÑÊÕ²Ø@2x.png'){
-        $(this).attr('src','image/icon_ÊÕ²Ø@2x.png');
+        operate_type = 'like';
+        comment_text='';
+        status = 'Y';
+    }else if (src =='image/icon_ç‚¹èµ_å·²ç‚¹èµ@2x.png'){
+        $(this).attr('src','image/icon_ç‚¹èµ@2x.png');
         var val = $(this).next('.num').text();
         $(this).next('.num').text(parseInt(val)-1);
+        operate_type = 'like';
+        comment_text='';
+        status = 'N';
     }
+    //    è¯„è®º
+    if(src =='image/icon_è¯„è®º@2x.png'){
+        alert('æš‚æ— è¯¥åŠŸèƒ½')
+        operate_type = 'comment';
+        comment_text='';  //æš‚æ— å†…å®¹æš‚æ— å†…å®¹æš‚æ— å†…å®¹æš‚æ— å†…å®¹æš‚æ— å†…å®¹
+        status = '';
+    }
+    //    æ”¶è—
+    if(src =='image/icon_æ”¶è—@2x.png'){
+        $(this).attr('src','image/icon_æ”¶è—_å·²æ”¶è—@2x.png');
+        var val = $(this).next('.num').text();
+        $(this).next('.num').text(parseInt(val)+1);
+        operate_type = 'collect';
+        comment_text='';
+        status = 'Y';
+    }else if (src =='image/icon_æ”¶è—_å·²æ”¶è—@2x.png'){
+        $(this).attr('src','image/icon_æ”¶è—@2x.png');
+        var val = $(this).next('.num').text();
+        $(this).next('.num').text(parseInt(val)-1);
+        operate_type = 'collect';
+        comment_text='';
+        status = 'N';
+    }
+    var param={};
+    param["corp_code"]=corp_code;
+    param["d_match_code"]=d_match_code;
+    param["operate_userCode"]=operate_userCode;
+    param["operate_type"]=operate_type;
+    param["status"]=status;
+    param["comment_text"]=comment_text;
+    oc.postRequire("post","/api/shopMatch/addRelByType","0",param,function(data){
+        if (data.code == "0") {
+            console.log(data);
+            //pageVal(num);
+        }else if(data.code =='-1'){
+            alert(data);
+        }
+    })
 });
 window.onload = function () {
     setInterval(function () {
