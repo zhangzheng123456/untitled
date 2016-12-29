eb1d1650e42c65b13aa6592239208b2e78c39057
zhou <781174667@qq.com>
2016-12-29 18:00:25 +0800

    no message

--- iShop/src/main/java/com/bizvane/ishop/controller/VipAnalysisController.java
+++ iShop/src/main/java/com/bizvane/ishop/controller/VipAnalysisController.java
@@ -1,5 +1,6 @@
 package com.bizvane.ishop.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.bizvane.ishop.bean.DataBean;
 import com.bizvane.ishop.constant.Common;
@@ -315,6 +316,220 @@ public class VipAnalysisController {
             dataBean.setCode(Common.DATABEAN_CODE_SUCCESS);
             dataBean.setId(id);
             dataBean.setMessage(result);
+        } catch (Exception ex) {
+            dataBean.setCode(Common.DATABEAN_CODE_ERROR);
+            dataBean.setId(id);
+            dataBean.setMessage(ex.getMessage());
+        }
+        return dataBean.getJsonStr();
+    }
+
+    //生日会员
+    @RequestMapping(value = "/vipChart", method = RequestMethod.POST)
+    @ResponseBody
+    public String vipChart(HttpServletRequest request) {
+        DataBean dataBean = new DataBean();
+        String corp_code = request.getSession().getAttribute("corp_code").toString();
+        String role_code = request.getSession().getAttribute("role_code").toString();
+        try {
+            String param = request.getParameter("param");
+            logger.info("json---------------" + param);
+            JSONObject jsonObj = JSONObject.parseObject(param);
+            id = jsonObj.get("id").toString();
+            String message = jsonObj.get("message").toString();
+            JSONObject jsonObject = JSONObject.parseObject(message);
+
+            if (role_code.equals(Common.ROLE_SYS))
+                corp_code = jsonObject.get("corp_code").toString();
+
+            String query_type = jsonObject.get("type").toString();
+            if (query_type.equals("analysis")){
+                String brand_code = jsonObject.get("brand_code").toString();
+                String area_code = jsonObject.get("area_code").toString();
+                String store_code = jsonObject.get("store_code").toString();
+                String vip_group_code = jsonObject.get("vip_group_code").toString();
+            }else if (query_type.equals("vipInfo")){
+                String vip_id = jsonObject.get("vip_id").toString();
+            }else if (query_type.equals("vipGroup")){
+                String vip_group_code = jsonObject.get("vip_group_code").toString();
+            }
+
+
+
+            //月份
+            JSONObject Month_obj = new JSONObject();
+            JSONArray Month_amt_array = new JSONArray();
+            JSONArray Month_num_array = new JSONArray();
+            String month = "一月,二月,三月,四月,五月,六月,七月,八月,九月,十月,十一月,十二月";
+            String[] months = month.split(",");
+            for (int i = 0; i < months.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",months[i]);
+                obj.put("value",i+30);
+                Month_amt_array.add(obj);
+            }
+            for (int i = 0; i < months.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",months[i]);
+                obj.put("value",i+2);
+                Month_num_array.add(obj);
+            }
+            Month_obj.put("trade_amt",Month_amt_array);
+            Month_obj.put("trade_num",Month_num_array);
+
+            //周
+            JSONObject Week_obj = new JSONObject();
+            JSONArray Week_amt_array = new JSONArray();
+            JSONArray Week_num_array = new JSONArray();
+
+            String week = "周一,周二,周三,周四,周五,周六,周日";
+            String[] weeks = week.split(",");
+            for (int i = 0; i < weeks.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",weeks[i]);
+                obj.put("value",i+30);
+                Week_amt_array.add(obj);
+            }
+            for (int i = 0; i < weeks.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",weeks[i]);
+                obj.put("value",i+2);
+                Week_num_array.add(obj);
+            }
+            Week_obj.put("trade_amt",Week_amt_array);
+            Week_obj.put("trade_num",Week_num_array);
+            
+            
+            //时段
+            JSONObject Time_obj = new JSONObject();
+            JSONArray Time_amt_array = new JSONArray();
+            JSONArray Time_num_array = new JSONArray();
+
+            String time = "10,12,14,16,18,20,22";
+            String[] times = time.split(",");
+            for (int i = 0; i < times.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",times[i]);
+                obj.put("value",i+30);
+                Time_amt_array.add(obj);
+            }
+            for (int i = 0; i < times.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",times[i]);
+                obj.put("value",i+2);
+                Time_num_array.add(obj);
+            }
+            Time_obj.put("trade_amt",Time_amt_array);
+            Time_obj.put("trade_num",Time_num_array);
+
+
+            //消费类型
+            JSONObject type_obj = new JSONObject();
+            JSONArray type_amt_array = new JSONArray();
+            JSONArray type_num_array = new JSONArray();
+
+            String type = "类型1,类型2,类型3,类型4,类型5,类型6,类型7,其他";
+            String[] types = type.split(",");
+            for (int i = 0; i < types.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",types[i]);
+                obj.put("value",i+30);
+                type_amt_array.add(obj);
+            }
+            for (int i = 0; i < types.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",types[i]);
+                obj.put("value",i+2);
+                type_num_array.add(obj);
+            }
+            type_obj.put("trade_amt",type_amt_array);
+            type_obj.put("trade_num",type_num_array);
+
+
+            //系列
+            JSONObject series_obj = new JSONObject();
+            JSONArray series_amt_array = new JSONArray();
+            JSONArray series_num_array = new JSONArray();
+
+            String series = "系列1,系列2,系列3,系列4,系列5,其他";
+            String[] seriess = series.split(",");
+            for (int i = 0; i < seriess.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",seriess[i]);
+                obj.put("value",i+30);
+                series_amt_array.add(obj);
+            }
+            for (int i = 0; i < seriess.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",seriess[i]);
+                obj.put("value",i+2);
+                series_num_array.add(obj);
+            }
+            series_obj.put("trade_amt",series_amt_array);
+            series_obj.put("trade_num",series_num_array);
+
+
+            //价格
+            JSONObject price_obj = new JSONObject();
+            JSONArray price_amt_array = new JSONArray();
+            JSONArray price_num_array = new JSONArray();
+
+            String price = "200以下,200-500,500-800,800-1200,1200-2000,2000-4000,4000以上";
+            String[] prices = price.split(",");
+            for (int i = 0; i < prices.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",prices[i]);
+                obj.put("value",i+30);
+                price_amt_array.add(obj);
+            }
+            for (int i = 0; i < prices.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",prices[i]);
+                obj.put("value",i+2);
+                price_num_array.add(obj);
+            }
+            price_obj.put("trade_amt",price_amt_array);
+            price_obj.put("trade_num",price_num_array);
+            
+            
+            //地域
+            JSONObject area_obj = new JSONObject();
+            JSONArray area_amt_array = new JSONArray();
+            JSONArray area_num_array = new JSONArray();
+
+            String area = "北京,西安,重庆,四川,上海,南京,广州,深圳";
+            String[] areas = area.split(",");
+            for (int i = 0; i < areas.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",areas[i]);
+                obj.put("value",i+30);
+                area_amt_array.add(obj);
+            }
+            for (int i = 0; i < areas.length; i++) {
+                JSONObject obj = new JSONObject();
+                obj.put("name",areas[i]);
+                obj.put("value",i+2);
+                area_num_array.add(obj);
+            }
+            area_obj.put("trade_amt",area_amt_array);
+            area_obj.put("trade_num",area_num_array);
+
+       
+
+
+            JSONObject chart_obj = new JSONObject();
+
+            chart_obj.put("Month",Month_obj);
+            chart_obj.put("Week",Week_obj);
+            chart_obj.put("Time",Time_obj);
+            chart_obj.put("Price",price_obj);
+            chart_obj.put("Series",series_obj);
+            chart_obj.put("Type",type_obj);
+            chart_obj.put("Area",area_obj);
+
+            dataBean.setCode(Common.DATABEAN_CODE_SUCCESS);
+            dataBean.setId(id);
+            dataBean.setMessage(chart_obj.toString());
         } catch (Exception ex) {
             dataBean.setCode(Common.DATABEAN_CODE_ERROR);
             dataBean.setId(id);
