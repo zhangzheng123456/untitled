50a569fa28b7d6933648efc35113cb78f0540431
huxue <huxue_pd@163.com>
2016-12-29 13:22:33 +0800

    js

--- iShop/src/main/webapp/goods/mobile/js/tie-inList.js
+++ iShop/src/main/webapp/goods/mobile/js/tie-inList.js
@@ -5,7 +5,7 @@ var oc = new ObjectControl();
 var corp_code = 'C10000';
 var pageNumber = '1';
 var pageSize = '20';
-var user_code = '';
+var user_code = '10000';
 
 //    选项卡-推荐
 $('.title div').eq(0).click(function () {
@@ -15,15 +15,6 @@ $('.title div').eq(0).click(function () {
     $('.title div').eq(0).css('background-color','white');
     $('.title div').eq(1).css('color','#888888');
     $('.title div').eq(1).css('background-color','#ededed');
-    var type = 'rec';
-    oc.postRequire("get", "/api/shopMatch/list?corp_code=" + corp_code +"&pageNumber=" + pageNumber + "&pageSize=" + pageSize+"&user_code="+user_code+"&type="+type+"", "0", "", function (data) {
-        if (data.code == "0") {
-            console.log(data);
-            //pageVal(num);
-        }else if(data.code =='-1'){
-            alert(data);
-        }
-    });
 });
 //    选项卡-我的
 $('.title div').eq(1).click(function () {
@@ -33,38 +24,94 @@ $('.title div').eq(1).click(function () {
     $('.title div').eq(1).css('background-color','white');
     $('.title div').eq(0).css('color','#888888');
     $('.title div').eq(0).css('background-color','#ededed');
-    var type = 'my';
-    oc.postRequire("get", "/api/shopMatch/list?corp_code=" + corp_code + "pageNumber=" + pageNumber + "pageSize=" + pageSize+"user_code="+user_code+"type="+type+"", "0", "", function (data) {
+});
+//推荐列表
+function getRec(){
+    var type = 'rec';
+    oc.postRequire("get", "/api/shopMatch/list?corp_code=" + corp_code +"&pageNumber=" + pageNumber + "&pageSize=" + pageSize+"&user_code="+user_code+"&type="+type+"", "0", "", function (data) {
         if (data.code == "0") {
-            console.log(data);
-            //pageVal(num);
+            var message = JSON.parse(data.message);
+            var list = message.list;
+            pageVal(list,type);
         }else if(data.code =='-1'){
             console.log(data);
         }
     });
-});
-
+}
+//我的列表
+function getMy(){
+    var type = 'my';
+    oc.postRequire("get", "/api/shopMatch/list?corp_code=" +corp_code +"&pageNumber=" + pageNumber + "&pageSize=" + pageSize+"&user_code="+user_code+"&type="+type+"", "0", "", function (data) {
+        if (data.code == "0") {
+            var message = JSON.parse(data.message);
+            var list = message.list;
+            pageVal(list,type);
+        }else if(data.code =='-1'){
+            console.log(data);
+        }
+    });
+}
 //页面加载获取数据
-function pageVal(num){
-    var tempHTML = '<ul class="goods_box" id="${id}" title="${id}"> <li class="the_img"><img src="${img}" alt=""/></li> <li class="the_list"> <img src="${img}" alt=""/> <img src="${img}" alt=""/> <img src="${img}" alt=""/> <span class="num">${num}</span> </li> <li class="add"> <div><img src="image/icon_点赞@2x.png" alt="点赞"/><span class="add_num">${num}</span></div> <div><img src="image/icon_评论@2x.png" alt="评论"/><span class="add_num">${num}</span></div> <div><img src="image/icon_收藏@2x.png" alt="收藏"/><span class="add_num">${num}</span></div> </li> </ul>';
+function  pageVal(list,type){
+    var tempHTML = '<ul class="goods_box" id="${d_match_code}" title="${d_match_title}"> <li class="the_img"><img src="${d_match_image}" alt=""/></li> <li class="the_list"> ';
+    var tempHTML2 =' <img src="${r_match_goodsImage}" alt="" id="${r_match_goodsCode}"/>';
+    var tempHTML3 =' <span class="num">${num}</span> </li> <li class="add"> <div><img src="image/icon_点赞@2x.png" alt="点赞"/><span class="add_num">${num}</span></div> <div><img src="image/icon_评论@2x.png" alt="评论"/><span class="add_num">${num}</span></div> <div><img src="image/icon_收藏@2x.png" alt="收藏"/><span class="add_num">${num}</span></div> </li> </ul>';
     var html = ''
-    for(i=0;i<10;i++){
+    for(i=0;i<list.length;i++){
+        var d_match_code = list[i].d_match_code;
+        var d_match_title = list[i].d_match_title;
+        var d_match_image = list[i].d_match_image;
+        var d_match_image_num = d_match_image.indexOf(",");
+        console.log(d_match_image_num)
+        //多张图取首张
+        if(d_match_image_num>=0){
+            d_match_image = d_match_image.substr(0,d_match_image_num);
+        }
+        var r_match_goods = list[i].r_match_goods;
         var nowHTML = tempHTML;
-        nowHTML = nowHTML.replace('${img}','image/list_item_00.jpg');
-        nowHTML = nowHTML.replace('${img}','image/list_item_01.jpg');
-        nowHTML = nowHTML.replace('${img}','image/list_item_02.jpg');
-        nowHTML = nowHTML.replace('${img}','image/list_item_02.jpg');
-        nowHTML = nowHTML.replace('${num}',num);
-        nowHTML = nowHTML.replace('${num}',num);
-        nowHTML = nowHTML.replace('${num}',num);
-        nowHTML = nowHTML.replace('${num}',num);
-        nowHTML = nowHTML.replace('${id}','id_'+i);
-        nowHTML = nowHTML.replace('${id}','id_'+i);
+        nowHTML = nowHTML.replace('${d_match_code}',d_match_code);
+        nowHTML = nowHTML.replace('${d_match_title}',d_match_title);
+        nowHTML = nowHTML.replace('${d_match_image}',d_match_image);
         html+= nowHTML;
-        $('.main').eq(0).html(html);
-        if(i<2){
+        //小图标
+        for(k=0;k<r_match_goods.length;k++){
+            if(k<3){
+                var r_match_goodsImage = r_match_goods[k].r_match_goodsImage;
+                var r_match_goodsCode = r_match_goods[k].r_match_goodsCode;
+                var nowHTML2 = tempHTML2;
+                nowHTML2 = nowHTML2.replace('${r_match_goodsImage}',r_match_goodsImage);//列表图片
+                nowHTML2 = nowHTML2.replace('${r_match_goodsCode}',r_match_goodsCode);//列表图片
+                html += nowHTML2;
+            }
+        }
+        var d_match_likeCount = list[i].d_match_likeCount;
+        var d_match_commentCount = list[i].d_match_commentCount;
+        var d_match_collectCount = list[i].d_match_collectCount;
+        var nowHTML3 = tempHTML3;
+        nowHTML3 = nowHTML3.replace('${num}',r_match_goods.length); //搭配商品件数
+        nowHTML3 = nowHTML3.replace('${num}',d_match_likeCount); //点赞数
+        nowHTML3 = nowHTML3.replace('${num}',d_match_commentCount); //评论数
+        nowHTML3 = nowHTML3.replace('${num}',d_match_collectCount); //收藏数
+        html += nowHTML3;
+        if(type=='rec'){
+            $('.main').eq(0).html(html);
+        }else if(type =='my'){
             $('.main').eq(1).html(html);
         }
+        var like_status = list[i].like_status; //点赞
+        var collect_status = list[i].collect_status; //收藏
+        click();
+        if(like_status=='Y'){
+            $('.add div img').eq(0).attr('src','image/icon_点赞_已点赞@2x.png');
+        }else if(like_status=='N'){
+            $('.add div img').eq(0).attr('src','image/icon_点赞@2x.png');
+        }
+        if(collect_status=='Y'){
+            $('.add div img').eq(2).attr('src','image/icon_收藏_已收藏@2x.png');
+        }else if(collect_status=='N'){
+            $('.add div img').eq(2).attr('src','image/icon_收藏@2x.png');
+        }
+        toNext();
     }
 }
 //控制宽高
@@ -76,40 +123,74 @@ function setTime(){
     $('.the_list img').css('height',listVal);
     $('.the_list').css('height',listVal);
 }
-
-window.onload = function () {
-    //默认推荐
-    $('.title div').eq(0).click();
-    //控制宽高
-    setTime()
+//点击事件-点赞-评论-收藏
+function  click(){
     $('.add div img').click(function () {
         var src = $(this).attr("src");
-        //    点赞
+        var corp_code = 'C10000';
+        var d_match_code = $(this).parents('.goods_box').attr('id');  //秀搭编号
+        var operate_userCode = '10000'; //操作人user_code
+        var operate_type = '';//type
+        var comment_text = '';//评论内容
+        var status = '';  //是否点赞or收藏
+        var status
         if(src =='image/icon_点赞@2x.png'){
             $(this).attr('src','image/icon_点赞_已点赞@2x.png');
             var val = $(this).next('.add_num').text();
             $(this).next('.add_num').text(parseInt(val)+1);
+            operate_type = 'like';
+            comment_text='';
+            status = 'Y';
         }else if (src =='image/icon_点赞_已点赞@2x.png'){
             $(this).attr('src','image/icon_点赞@2x.png');
             var val = $(this).next('.add_num').text();
             $(this).next('.add_num').text(parseInt(val)-1);
+            operate_type = 'like';
+            comment_text='';
+            status = 'N';
         }
         //    评论
         if(src =='image/icon_评论@2x.png'){
             alert('暂无该功能')
+            //operate_type = 'comment';
+            //comment_text='';  //暂无内容暂无内容暂无内容暂无内容暂无内容
+            //status = '';
         }
         //    收藏
         if(src =='image/icon_收藏@2x.png'){
             $(this).attr('src','image/icon_收藏_已收藏@2x.png');
             var val = $(this).next('.add_num').text();
             $(this).next('.add_num').text(parseInt(val)+1);
+            operate_type = 'collect';
+            comment_text='';
+            status = 'Y';
         }else if (src =='image/icon_收藏_已收藏@2x.png'){
             $(this).attr('src','image/icon_收藏@2x.png');
             var val = $(this).next('.add_num').text();
             $(this).next('.add_num').text(parseInt(val)-1);
+            operate_type = 'collect';
+            comment_text='';
+            status = 'N';
         }
+        var param={};
+        param["corp_code"]=corp_code;
+        param["d_match_code"]=d_match_code;
+        param["operate_userCode"]=operate_userCode;
+        param["operate_type"]=operate_type;
+        param["status"]=status;
+        param["comment_text"]=comment_text;
+        oc.postRequire("post","/api/shopMatch/addRelByType","0",param,function(data){
+            if (data.code == "0") {
+                console.log(data);
+                //pageVal(num);
+            }else if(data.code =='-1'){
+                //alert(data);
+            }
+        })
     });
-    //跳转
+}
+//跳转
+function toNext(){
     $('.the_img img').click(function () {
         window.location = 'details.html';
     })
@@ -117,3 +198,38 @@ window.onload = function () {
         window.location = 'details.html';
     })
 }
+window.onload = function () {
+    //默认推荐
+    $('.title div').eq(0).click();
+    //控制宽高
+    setTime();
+    //获取推荐
+    getRec();
+    //获取我的
+    getMy();
+}
+//var src = $(this).attr("src");
+////    点赞
+//if(src =='image/icon_点赞@2x.png'){
+//    $(this).attr('src','image/icon_点赞_已点赞@2x.png');
+//    var val = $(this).next('.add_num').text();
+//    $(this).next('.add_num').text(parseInt(val)+1);
+//}else if (src =='image/icon_点赞_已点赞@2x.png'){
+//    $(this).attr('src','image/icon_点赞@2x.png');
+//    var val = $(this).next('.add_num').text();
+//    $(this).next('.add_num').text(parseInt(val)-1);
+//}
+////    评论
+//if(src =='image/icon_评论@2x.png'){
+//    alert('暂无该功能')
+//}
+////    收藏
+//if(src =='image/icon_收藏@2x.png'){
+//    $(this).attr('src','image/icon_收藏_已收藏@2x.png');
+//    var val = $(this).next('.add_num').text();
+//    $(this).next('.add_num').text(parseInt(val)+1);
+//}else if (src =='image/icon_收藏_已收藏@2x.png'){
+//    $(this).attr('src','image/icon_收藏@2x.png');
+//    var val = $(this).next('.add_num').text();
+//    $(this).next('.add_num').text(parseInt(val)-1);
+//}
