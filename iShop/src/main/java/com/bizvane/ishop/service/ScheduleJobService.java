39791f09e220daf02efeab2581424eec756c6637
zhou <781174667@qq.com>
2017-01-05 20:06:11 +0800

    no message

---
+++ iShop/src/main/java/com/bizvane/ishop/service/ScheduleJobService.java
@@ -0,0 +1,94 @@
+package com.bizvane.ishop.service;
+
+import com.bizvane.ishop.entity.ScheduleJob;
+
+/**
+ * Bizvane
+ * createTime : 2016-08-04
+ * description : 定时任务服务
+ * version : 1.0
+ */
+public interface ScheduleJobService {
+
+    /**
+     * 初始化定时任务
+     */
+    public void initScheduleJob() throws Exception;
+
+    /**
+     * 新增
+     * 
+     * @param scheduleJob
+     * @return
+     */
+    public int insert(ScheduleJob scheduleJob) throws Exception;
+
+    /**
+     * 直接修改 只能修改运行的时间，参数、同异步等无法修改
+     * 
+     * @param scheduleJob
+     */
+    public void update(ScheduleJob scheduleJob) throws Exception;
+
+    /**
+     * 删除重新创建方式
+     * 
+     * @param scheduleJob
+     */
+    public void delUpdate(ScheduleJob scheduleJob) throws Exception;
+
+    /**
+     * 删除
+     * 
+     * @param scheduleJobId
+     */
+    public void delete(int scheduleJobId) throws Exception;
+
+    /**
+     * 运行一次任务
+     *
+     * @param scheduleJobId the schedule job id
+     * @return
+     */
+    public void runOnce(int scheduleJobId) throws Exception;
+
+    /**
+     * 暂停任务
+     *
+     * @param scheduleJobId the schedule job id
+     * @return
+     */
+    public void pauseJob(int scheduleJobId) throws Exception;
+
+    /**
+     * 恢复任务
+     *
+     * @param scheduleJobId the schedule job id
+     * @return
+     */
+    public void resumeJob(int scheduleJobId) throws Exception;
+
+    /**
+     * 获取任务对象
+     * 
+     * @param scheduleJobId
+     * @return
+     */
+    public ScheduleJob get(int scheduleJobId) throws Exception;
+
+    /**
+     * 查询任务列表
+     * 
+     * @param scheduleJobVo
+     * @return
+     */
+//    public List<ScheduleJobVo> queryList(ScheduleJobVo scheduleJobVo);
+
+    /**
+     * 获取运行中的任务列表
+     *
+     * @return
+     */
+//    public List<ScheduleJobVo> queryExecutingJobList();
+
+}
--- iShop/src/test/java/com/bizvane/ishop/service/Test01.java
+++ iShop/src/test/java/com/bizvane/ishop/service/Test01.java
@@ -1,11 +1,19 @@
 package com.bizvane.ishop.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.bizvane.ishop.entity.User;
+import com.bizvane.ishop.utils.IshowHttpClient;
+import com.bizvane.sun.v1.common.DataBox;
+import org.apache.avro.data.Json;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.*;
 
 /**
  * Created by yanyadong on 2016/11/23.
@@ -20,13 +28,31 @@ public class Test01 {
 
       @Autowired
       UserService service;
-
+    @Autowired
+    IceInterfaceService iceInterfaceService;
 
     @Test
     public  void showUser(){
         try {
-            User user = service.getUserById(10219);
-            System.out.println(user.getUser_code()+"  "+user.getPassword());
+//            User user = service.getUserById(10219);
+//            System.out.println(user.getUser_code()+"  "+user.getPassword());
+
+            JSONArray array = new JSONArray();
+
+            JSONObject obj = new JSONObject();
+            obj.put("corp_code","11");
+            obj.put("vip_id","22");
+            obj.put("vip_iname","22");
+            array.add(obj);
+
+            obj.put("corp_code","11");
+            obj.put("vip_id","22");
+            obj.put("vip_iname","22");
+            array.add(obj);
+
+            System.out.println("======"+ JSON.toJSONString(array));
+
+
         }
         catch(Exception e){
             e.printStackTrace();
