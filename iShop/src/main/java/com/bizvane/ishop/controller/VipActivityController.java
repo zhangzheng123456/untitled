e018befd3b63e974f6927dc01b0ff78e31c16ecb
jiangyan <1042034123@qq.com>
2017-01-10 14:43:08 +0800

    修改

--- iShop/src/main/java/com/bizvane/ishop/controller/VipActivityController.java
+++ iShop/src/main/java/com/bizvane/ishop/controller/VipActivityController.java
@@ -1,4 +1,4 @@
-package com.bizvane.ishop.controller.v2;
+package com.bizvane.ishop.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -6,8 +6,6 @@ import com.bizvane.ishop.bean.DataBean;
 import com.bizvane.ishop.constant.Common;
 import com.bizvane.ishop.entity.Task;
 import com.bizvane.ishop.entity.VipActivity;
-import com.bizvane.ishop.entity.VipActivityDetail;
-import com.bizvane.ishop.entity.VipFsend;
 import com.bizvane.ishop.service.TaskService;
 import com.bizvane.ishop.service.VipActivityService;
 import com.bizvane.ishop.service.VipFsendService;
@@ -22,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.Map;
 
 /**
