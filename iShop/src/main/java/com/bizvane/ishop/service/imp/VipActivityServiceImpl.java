f06e8a7362f07c04723aec449b47d8a1b31b69a0
jiangyan <1042034123@qq.com>
2017-01-11 12:23:47 +0800

    修改

--- iShop/src/main/java/com/bizvane/ishop/service/imp/VipActivityServiceImpl.java
+++ iShop/src/main/java/com/bizvane/ishop/service/imp/VipActivityServiceImpl.java
@@ -144,7 +144,6 @@ public class VipActivityServiceImpl implements VipActivityService {
             info = vipActivityMapper.updateActivity(vipActivity);
             if (info > 0) {
                 result = Common.DATABEAN_CODE_SUCCESS;
-
             } else {
                 result = "编辑失败";
 
