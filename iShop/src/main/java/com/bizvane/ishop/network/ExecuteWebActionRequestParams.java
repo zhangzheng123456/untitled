f073bf4ac3658a6e158d589599b249e21f2cd021
yadong <136531660@qq.com>
2017-01-15 17:11:49 +0800

    no message

--- iShop/src/main/java/com/bizvane/ishop/network/ExecuteWebActionRequestParams.java
+++ iShop/src/main/java/com/bizvane/ishop/network/ExecuteWebActionRequestParams.java
@@ -1,12 +1,9 @@
 package com.bizvane.ishop.network;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ExecuteWebActionRequestParams  extends  RequestParams{
-
-	private String transactions; // 一个transaction里的多个操作将全部成功，或全部失败
 
 	public class ExecuteWebActionTrans  extends BaseTrans{
 
@@ -47,22 +44,5 @@ public class ExecuteWebActionRequestParams  extends  RequestParams{
 		}
 	}
 
-	public String getTransactions() {
-		return transactions;
-	}
 
-	public void setTransactions(BaseTrans transaction) throws JSONException {
-
-		JSONArray jSONTransactions = new JSONArray();
-		JSONObject jSONTransaction = new JSONObject();
-
-		jSONTransaction.put("id", transaction.getId());
-		jSONTransaction.put("command", transaction.getCommand());
-		jSONTransaction.put("params", transaction.getParams());
-
-		jSONTransactions.put(jSONTransaction);
-
-		this.transactions = jSONTransactions.toString();
-
-	}
 }
