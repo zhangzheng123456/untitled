6740d5d10e2e78d55c6e839bbb36fd8b527aa1e6
huxue <huxue_pd@163.com>
2017-01-18 11:58:30 +0800

    卡号获取

--- iShop/src/main/webapp/js/vip/vip_dataPage.js
+++ iShop/src/main/webapp/js/vip/vip_dataPage.js
@@ -382,7 +382,7 @@ $('#toSave').click(function(){
     var topType = $('#execution_input').val(); //充值类型
     var topUpShop = $('#topUpShop').val();  //充值店仓
     var topUpPeople = $('#topUpPeople').val();//经办人
-    var topUpCard = $('#vip_card_no').val();//会员卡号
+    var topUpCard = $('#vip_card_no').text();//会员卡号
     var topUpVipName = $('#topUpVipName').val();
     var topUpMoney = $('#topUpMoney').val(); //吊牌金额
     var topUpMoneyReality = $('#topUpMoneyReality').val(); //实付金额
@@ -461,7 +461,7 @@ function toSave(){
     param["corp_code"] = sessionStorage.getItem("corp_code");//企业编号
     param["vip_id"] = sessionStorage.getItem("id");//会员编号
     param["vip_name"] = $('#vip_name').text();//会员名称
-    param["card_no"] = $('#vip_card_no').val();//会员卡号
+    param["card_no"] = $('#vip_card_no').text();//会员卡号
     param["remark"] = $('#refundNote').val();//备注
     param["store_name"] = $('#refunShop').val();//充值店铺
     param["store_code"] = sessionStorage.getItem("store_id");//店铺编号
